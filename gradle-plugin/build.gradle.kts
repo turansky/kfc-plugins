5c333e887afa04717c79ee0cdd2e6631915ba832
Victor Turansky <victor.turansky@gmail.com>
2020-02-11 11:46:25 +0300

    Simple plugin configuration

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,4 +1,54 @@
-tasks.wrapper {
-    gradleVersion = "6.1.1"
-    distributionType = Wrapper.DistributionType.ALL
+plugins {
+    id("java-gradle-plugin")
+    id("com.gradle.plugin-publish") version "0.10.1"
+    id("org.gradle.kotlin.kotlin-dsl") version "1.3.3"
+
+    kotlin("jvm") version "1.3.61"
+}
+
+repositories {
+    jcenter()
+}
+
+kotlinDslPluginOptions {
+    experimentalWarning.set(false)
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            allWarningsAsErrors = true
+        }
+    }
+
+    jar {
+        into("META-INF") {
+            from("$projectDir/LICENSE.md")
+        }
+    }
+
+    val preparePublish by registering {
+        doLast {
+            preparePublish()
+        }
+    }
+
+    val prepareDevelopment by registering {
+        doLast {
+            prepareDevelopment()
+        }
+    }
+
+    wrapper {
+        gradleVersion = "6.1.1"
+        distributionType = Wrapper.DistributionType.ALL
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(gradleApi())
+
+    compileOnly(kotlin("gradle-plugin"))
 }
