d065b5431ce3e141c2cf8ac77293fcb7e6cb9a48
Victor Turansky <victor.turansky@gmail.com>
2020-02-12 01:13:57 +0300

    Empty version plugin

---
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/VersionPlugin.kt
@@ -0,0 +1,10 @@
+package com.github.turansky.kfc.gradle.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class VersionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        // implement
+    }
+}
