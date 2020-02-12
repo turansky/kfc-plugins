6a271bfbf7aa45912e7c6d03a35a7b76a558ad45
Victor Turansky <victor.turansky@gmail.com>
2020-02-12 03:39:15 +0300

    Add Kotlin library configuration

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/LibraryPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/LibraryPlugin.kt
@@ -2,9 +2,30 @@ package com.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
+import org.jetbrains.kotlin.gradle.tasks.KotlinJsDce
+
+private val RUN_TASKS = setOf(
+    "browserDevelopmentRun"
+)
 
 class LibraryPlugin : Plugin<Project> {
-    override fun apply(target: Project) {
-        // implement
-    }
+    override fun apply(target: Project): Unit =
+        with(target) {
+            tasks {
+                withType<KotlinJsDce>().configureEach {
+                    enabled = false
+                }
+
+                withType<KotlinWebpack>().configureEach {
+                    if (name !in RUN_TASKS) {
+                        enabled = false
+                    }
+
+                    sourceMaps = false
+                }
+            }
+        }
 }
