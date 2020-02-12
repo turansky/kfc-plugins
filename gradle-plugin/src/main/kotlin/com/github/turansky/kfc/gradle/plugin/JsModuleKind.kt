6bbbb364bdf388ece24548e8c830f1b2d1734434
Victor Turansky <victor.turansky@gmail.com>
2020-02-12 03:48:01 +0300

    CommonJS - default module kind for Kotlin/JS library

---
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/JsModuleKind.kt
@@ -0,0 +1,5 @@
+package com.github.turansky.kfc.gradle.plugin
+
+internal object JsModuleKind {
+    val COMMON_JS = "commonjs"
+}
--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/LibraryPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/LibraryPlugin.kt
@@ -1,9 +1,12 @@
 package com.github.turansky.kfc.gradle.plugin
 
+import com.github.turansky.kfc.gradle.plugin.JsModuleKind.COMMON_JS
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+import org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 import org.jetbrains.kotlin.gradle.tasks.KotlinJsDce
 
@@ -25,6 +28,14 @@ class LibraryPlugin : Plugin<Project> {
                     }
 
                     sourceMaps = false
+                }
+            }
+
+            plugins.withType<KotlinJsPluginWrapper> {
+                tasks.withType<KotlinJsCompile>().configureEach {
+                    kotlinOptions {
+                        moduleKind = COMMON_JS
+                    }
                 }
             }
         }
