f5b89aedeed40283585e42040241c737b9a231b1
Victor Turansky <victor.turansky@gmail.com>
2020-02-13 01:41:59 +0300

    Maven publication for Kotlin/JS

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/MavenPublishPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/MavenPublishPlugin.kt
@@ -2,9 +2,28 @@ package com.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.jvm.tasks.Jar
+import org.gradle.kotlin.dsl.*
+import org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin as StandardMavenPublishPlugin
 
 class MavenPublishPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
-        // implement
+        plugins.withType<KotlinJsPluginWrapper> {
+            plugins.apply(StandardMavenPublishPlugin::class)
+
+            plugins.withType<StandardMavenPublishPlugin> {
+                configure<PublishingExtension> {
+                    publications {
+                        register("mavenKotlin", MavenPublication::class) {
+                            from(components["kotlin"])
+                            artifact(tasks.named<Jar>("JsSourcesJar").get())
+                        }
+                    }
+                }
+            }
+        }
     }
 }
