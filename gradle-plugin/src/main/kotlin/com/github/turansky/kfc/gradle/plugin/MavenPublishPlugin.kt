df1a0116af3058e70ab1c3e1e60f923bfd461880
Victor Turansky <victor.turansky@gmail.com>
2020-02-13 01:19:31 +0300

    Empty maven publish plugin

---
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/MavenPublishPlugin.kt
@@ -0,0 +1,10 @@
+package com.github.turansky.kfc.gradle.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MavenPublishPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        // implement
+    }
+}
