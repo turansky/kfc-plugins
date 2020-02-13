725a38472f80dff6b14547fbb0b0b1ad1b3bbe8e
Victor Turansky <victor.turansky@gmail.com>
2020-02-13 11:06:06 +0300

    Common download base url

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/RootPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/RootPlugin.kt
@@ -8,21 +8,41 @@ import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.yarn
 
+private val DOWNLOAD_BASE_URL = "kfc.download.base.url"
+
+private val NODE_PATH = "nodejs.org/dist"
+private val YARN_PATH = "yarnpkg/yarn"
+
 private val NODE_DOWNLOAD_BASE_URL = "kfc.node.download.base.url"
 private val YARN_DOWNLOAD_BASE_URL = "kfc.yarn.download.base.url"
 
 class RootPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
-        if (hasProperty(NODE_DOWNLOAD_BASE_URL)) {
+        nodeDownloadBaseUrl()?.let { baseUrl ->
             plugins.withType<NodeJsRootPlugin> {
                 configure<NodeJsRootExtension> {
-                    nodeDownloadBaseUrl = property(NODE_DOWNLOAD_BASE_URL) as String
+                    nodeDownloadBaseUrl = baseUrl
                 }
             }
         }
 
-        if (hasProperty(YARN_DOWNLOAD_BASE_URL)) {
-            yarn.downloadBaseUrl = property(YARN_DOWNLOAD_BASE_URL) as String
+        yarnDownloadBaseUrl()?.let { baseUrl ->
+            yarn.downloadBaseUrl = baseUrl
         }
     }
 }
+
+private fun Project.nodeDownloadBaseUrl(): String? =
+    propertyOrNull(NODE_DOWNLOAD_BASE_URL)
+        ?: downloadBaseUrl(NODE_PATH)
+
+private fun Project.yarnDownloadBaseUrl(): String? =
+    propertyOrNull(YARN_DOWNLOAD_BASE_URL)
+        ?: downloadBaseUrl(YARN_PATH)
+
+private fun Project.downloadBaseUrl(suffix: String): String? =
+    propertyOrNull(DOWNLOAD_BASE_URL)
+        ?.let { "$it/$suffix" }
+
+private fun Project.propertyOrNull(s: String): String? =
+    if (hasProperty(s)) property(s) as String else null
