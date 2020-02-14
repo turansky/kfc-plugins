5797c92050f2f62a54ae66ca2eb6c629d71e1f70
Victor Turansky <victor.turansky@gmail.com>
2020-02-14 11:18:28 +0300

    Implement plugin version plugin

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/PluginVersionPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/PluginVersionPlugin.kt
@@ -4,20 +4,41 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.invoke
 
+private const val GRADLE_PLUGIN_PREFIX = "gradle.plugin."
+
 class PluginVersionPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         tasks {
             register("preparePublish") {
                 doLast {
-                    // implement
+                    changeVersion(currentVersion.toRelease())
+                    changeGroup(addPrefix = false)
                 }
             }
 
             register("prepareDevelopment") {
                 doLast {
-                    // implement
+                    changeVersion(currentVersion.toNextSnapshot())
+                    changeGroup(addPrefix = true)
                 }
             }
         }
     }
 }
+
+private fun Project.changeGroup(addPrefix: Boolean) {
+    var group = group.toString()
+    group = if (addPrefix) {
+        "$GRADLE_PLUGIN_PREFIX$group"
+    } else {
+        group.removePrefix(GRADLE_PLUGIN_PREFIX)
+    }
+
+    setGradleProperty(GradleProperty.GROUP, group)
+}
+
+private fun Project.changeVersion(newVersion: Version) {
+    setGradleProperty(GradleProperty.VERSION, newVersion.toString())
+    version = newVersion.toString()
+}
+
