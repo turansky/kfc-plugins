659816a18ef575338e03f382463aeb5f0f379d1d
Victor Turansky <victor.turansky@gmail.com>
2020-02-15 01:56:26 +0300

    JVM 1.8 for plugin

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/PluginPublishPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/PluginPublishPlugin.kt
@@ -3,6 +3,8 @@ package com.github.turansky.kfc.gradle.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 
 private val GROUP_PREFIX = BooleanProperty("kfc.plugin.publish.group.prefix")
 
@@ -11,6 +13,13 @@ private const val GRADLE_PLUGIN_PREFIX = "gradle.plugin."
 class PluginPublishPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         tasks {
+            withType<KotlinJvmCompile> {
+                kotlinOptions {
+                    jvmTarget = "1.8"
+                    allWarningsAsErrors = true
+                }
+            }
+
             register("preparePublish") {
                 doLast {
                     changeVersion(currentVersion.toRelease())
