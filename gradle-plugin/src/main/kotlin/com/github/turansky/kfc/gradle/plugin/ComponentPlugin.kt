426971d8c2addb2e670ab28b958fc84f0406104d
Victor Turansky <victor.turansky@gmail.com>
2020-02-15 20:23:37 +0300

    Configure browser name via browser target

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ComponentPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ComponentPlugin.kt
@@ -3,12 +3,25 @@ package com.github.turansky.kfc.gradle.plugin
 import com.github.turansky.kfc.gradle.plugin.JsTarget.COMMONJS
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper
 
 class ComponentPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
+        configure<KotlinJsProjectExtension> {
+            target {
+                browser {
+                    webpackTask {
+                        outputFileName = COMPONENT_JS
+                        sourceMaps = false
+                    }
+                }
+            }
+        }
+
         plugins.withType<KotlinJsPluginWrapper> {
             tasks.withType<KotlinJsCompile> {
                 kotlinOptions {
