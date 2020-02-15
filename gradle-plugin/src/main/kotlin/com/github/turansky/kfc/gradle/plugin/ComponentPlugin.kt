3607267b302897856b8456eb62f935e44bc5d1dc
Victor Turansky <victor.turansky@gmail.com>
2020-02-15 21:03:31 +0300

    DCE configuration via component extension

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ComponentPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ComponentPlugin.kt
@@ -4,13 +4,21 @@ import com.github.turansky.kfc.gradle.plugin.JsTarget.COMMONJS
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsDce
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper
 
+open class ComponentExtension {
+    var root: String? = null
+}
+
 class ComponentPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
+        val extension = extensions.create<ComponentExtension>("component")
+
         configure<KotlinJsProjectExtension> {
             target {
                 browser {
@@ -18,6 +26,14 @@ class ComponentPlugin : Plugin<Project> {
                         outputFileName = COMPONENT_JS
                         sourceMaps = false
                     }
+                }
+            }
+        }
+
+        afterEvaluate {
+            tasks.withType<KotlinJsDce> {
+                extension.root?.let {
+                    keep += keepId(it)
                 }
             }
         }
