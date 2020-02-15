f78f955a2196f0c89ff3d912cb43f75fa19e2970
Victor Turansky <victor.turansky@gmail.com>
2020-02-15 17:06:33 +0300

    Add component extension

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ComponentPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ComponentPlugin.kt
@@ -3,19 +3,33 @@ package com.github.turansky.kfc.gradle.plugin
 import com.github.turansky.kfc.gradle.plugin.JsTarget.COMMONJS
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsDce
 import org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 
+open class ComponentExtension {
+    var root: String? = null
+}
+
 class ComponentPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
+        val extension = extensions.create<ComponentExtension>("component")
+
         plugins.withType<KotlinJsPluginWrapper> {
             tasks {
                 withType<KotlinWebpack> {
                     outputFileName = COMPONENT_JS
                     sourceMaps = false
+                }
+
+                withType<KotlinJsDce> {
+                    extension.root?.let {
+                        keep += keepId(it)
+                    }
                 }
 
                 withType<KotlinJsCompile> {
