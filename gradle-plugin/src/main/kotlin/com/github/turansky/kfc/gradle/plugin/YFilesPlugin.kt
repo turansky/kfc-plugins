6bedda588c47bfd4fc9a8bb44365cb972454da7b
Victor Turansky <victor.turansky@gmail.com>
2020-02-17 02:40:59 +0300

    Empty yFiles plugin

---
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/YFilesPlugin.kt
@@ -0,0 +1,10 @@
+package com.github.turansky.kfc.gradle.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class YFilesPlugin : Plugin<Project> {
+    override fun apply(target: Project): Unit = with(target) {
+        // implement
+    }
+}
