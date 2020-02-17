355a9e2cdd4001687afb5265f31fd6900b2204aa
Victor Turansky <victor.turansky@gmail.com>
2020-02-17 03:14:48 +0300

    Rules for yFiles

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/YFilesPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/YFilesPlugin.kt
@@ -4,17 +4,33 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.withType
 
+// language=JavaScript
 private val EXTERNALS = """
 config.externals = [
-    /^yfiles${'$'}/i,
-    /^yfiles\/.+${'$'}/i
+  /^yfiles${'$'}/i,
+  /^yfiles\/.+${'$'}/i
 ]
+""".trimIndent()
+
+// language=JavaScript
+private val RULES = """
+  config.module.rules.push(
+    {
+      test: /\.css${'$'}/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.svg${'$'}/,
+      use: 'svg-inline-loader'
+    }
+  )
 """.trimIndent()
 
 class YFilesPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         tasks.withType<WebpackConfigTask> {
             patch("externals", EXTERNALS)
+            patch("rules", RULES)
         }
     }
 }
