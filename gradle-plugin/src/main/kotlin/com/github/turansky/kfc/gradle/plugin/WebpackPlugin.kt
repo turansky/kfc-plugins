a34a7c1b0ea4ac55ab6cd746bf5be4c788c90e0e
Victor Turansky <victor.turansky@gmail.com>
2020-02-17 03:08:58 +0300

    Clean for webpack configs

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebpackPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebpackPlugin.kt
@@ -2,7 +2,9 @@ package com.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Delete
 import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
@@ -13,6 +15,10 @@ class WebpackPlugin : Plugin<Project> {
         plugins.withType<KotlinJsPluginWrapper> {
             tasks {
                 val patchWebpackConfig = register<WebpackConfigTask>("patchWebpackConfig")
+
+                named<Delete>("clean") {
+                    delete(patchWebpackConfig)
+                }
 
                 withType<KotlinJsCompile> {
                     dependsOn(patchWebpackConfig)
