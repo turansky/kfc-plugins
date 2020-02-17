9b8fa2fb1d55649d475a4e3ad3a2feff0ffa9d12
Victor Turansky <victor.turansky@gmail.com>
2020-02-17 04:25:39 +0300

    Optional implementation configuration

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Projects.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Projects.kt
@@ -3,9 +3,14 @@ package com.github.turansky.kfc.gradle.plugin
 import org.gradle.api.Project
 import org.gradle.api.internal.artifacts.dependencies.DefaultProjectDependency
 
+private val IMPLEMENTATION = "implementation"
+
 // TODO: optimize calculation
 internal fun Project.relatedProjects(): Set<Project> {
-    return configurations.getByName("implementation")
+    val configuration = configurations.findByName(IMPLEMENTATION)
+        ?: return emptySet()
+
+    return configuration
         .allDependencies
         .asSequence()
         .filterIsInstance<DefaultProjectDependency>()
