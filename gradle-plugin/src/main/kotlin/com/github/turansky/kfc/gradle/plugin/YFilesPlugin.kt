8531456998e35fc5296920716055aed46849bd4c
Victor Turansky <victor.turansky@gmail.com>
2020-02-17 03:01:00 +0300

    External yFiles

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/YFilesPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/YFilesPlugin.kt
@@ -2,9 +2,19 @@ package com.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.withType
+
+private val EXTERNALS = """
+config.externals = [
+    /^yfiles${'$'}/i,
+    /^yfiles\/.+${'$'}/i
+]
+""".trimIndent()
 
 class YFilesPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
-        // implement
+        tasks.withType<WebpackConfigTask> {
+            patch("externals", EXTERNALS)
+        }
     }
 }
