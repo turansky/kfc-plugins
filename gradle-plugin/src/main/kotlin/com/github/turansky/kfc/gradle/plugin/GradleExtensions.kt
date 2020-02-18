f38804809a67ffb0e950063590331ca4181091ba
Victor Turansky <victor.turansky@gmail.com>
2020-02-19 00:30:51 +0300

    Generic configure method

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/GradleExtensions.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/GradleExtensions.kt
@@ -1,11 +1,11 @@
 package com.github.turansky.kfc.gradle.plugin
 
-import org.gradle.api.Action
-import org.gradle.api.Task
-import org.gradle.api.tasks.TaskCollection
+import org.gradle.api.DomainObjectCollection
 import org.gradle.kotlin.dsl.withType
 
 // https://github.com/gradle/gradle/issues/12266
-internal inline fun <reified S : Task> TaskCollection<in S>.configureEach(action: Action<S>) {
-    withType<S>().configureEach(action)
+inline fun <reified S : Any> DomainObjectCollection<in S>.configureEach(
+    noinline action: S.() -> Unit
+) {
+    withType().configureEach(action)
 }
