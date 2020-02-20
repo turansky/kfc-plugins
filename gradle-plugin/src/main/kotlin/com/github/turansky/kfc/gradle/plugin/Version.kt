5ba0c39b3fcea19e00632f78877f4f6a8901da17
Victor Turansky <victor.turansky@gmail.com>
2020-02-21 02:05:02 +0300

    Add fixed version class

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Version.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Version.kt
@@ -3,21 +3,36 @@ package com.github.turansky.kfc.gradle.plugin
 private val SNAPSHOT_SUFFIX = "-SNAPSHOT"
 private val DELIMITER = "."
 
-internal fun parseVersion(source: String): Version {
+internal fun parseVersion(
+    source: String,
+    fixed: Boolean = false
+): Version {
     val version = source.removeSuffix(SNAPSHOT_SUFFIX)
+    val snapshot = version != source
 
     val parts = version
         .split(DELIMITER)
         .map { it.toInt() }
 
-    check(parts.size == 3)
+    return if (fixed) {
+        check(parts.size == if (snapshot) 3 else 4)
 
-    return StandardVersion(
-        major = parts[0],
-        minor = parts[1],
-        patch = parts[2],
-        snapshot = version != source
-    )
+        FixedVersion(
+            major = parts[0],
+            minor = parts[1],
+            patch = parts[2],
+            date = if (snapshot) null else parts[3]
+        )
+    } else {
+        check(parts.size == 3)
+
+        StandardVersion(
+            major = parts[0],
+            minor = parts[1],
+            patch = parts[2],
+            snapshot = snapshot
+        )
+    }
 }
 
 internal sealed class Version {
@@ -55,7 +70,24 @@ private data class StandardVersion(
             patch = patch + 1,
             snapshot = true
         )
+}
+
+private data class FixedVersion(
+    override val major: Int,
+    override val minor: Int,
+    override val patch: Int,
+    private val date: Int?
+) : Version() {
+    override val snapshot: Boolean = date == null
+
+    override fun toRelease(): Version =
+        copy(date = currentDate())
+
+    override fun toNextSnapshot(): Version =
+        copy(date = null)
 
     override fun toString(): String =
         super.toString()
 }
+
+private fun currentDate(): Int = TODO()
