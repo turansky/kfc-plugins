16ce79b42afc65a52d22477736f98e4ab834e67e
Victor Turansky <victor.turansky@gmail.com>
2020-02-28 00:49:00 +0300

    Simple WebComponentPlugin

---
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebComponentPlugin.kt
@@ -0,0 +1,56 @@
+package com.github.turansky.kfc.gradle.plugin
+
+import com.github.turansky.kfc.gradle.plugin.JsTarget.COMMONJS
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsDce
+import org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
+
+open class WebComponentExtension {
+    lateinit var id: String
+    lateinit var source: String
+}
+
+class WebComponentPlugin : Plugin<Project> {
+    override fun apply(target: Project): Unit = with(target) {
+        plugins.apply(WebpackPlugin::class)
+
+        val extension = extensions.create<WebComponentExtension>("component")
+
+        plugins.withType<KotlinJsPluginWrapper> {
+            tasks {
+                configureEach<KotlinJsCompile> {
+                    kotlinOptions {
+                        moduleKind = COMMONJS
+                    }
+                }
+
+                configureEach<KotlinWebpack> {
+                    outputFileName = COMPONENT_JS
+                    sourceMaps = false
+                }
+            }
+        }
+
+        afterEvaluate {
+            val componentRoot = extension.source
+                .substringBeforeLast(".")
+
+            tasks {
+                configureEach<KotlinJsDce> {
+                    keep += keepId(componentRoot)
+                }
+
+                configureEach<PatchWebpackConfig> {
+                    patch("output", defaultOutputConfiguration())
+                }
+            }
+        }
+    }
+}
