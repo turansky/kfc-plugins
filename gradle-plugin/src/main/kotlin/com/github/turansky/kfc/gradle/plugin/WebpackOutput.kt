ccd5bf44e3087324694e3ac6c4dfbfcffbb5f195
Victor Turansky <victor.turansky@gmail.com>
2020-02-28 00:42:16 +0300

    Add default output configuration

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebpackOutput.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebpackOutput.kt
@@ -1,11 +1,21 @@
 package com.github.turansky.kfc.gradle.plugin
 
+internal fun defaultOutputConfiguration(): String {
+    // language=JavaScript
+    return """
+            config.output = config.output || {}
+            config.output.libraryTarget = 'umd'
+            delete config.output.library
+    """.trimIndent()
+}
+
 internal fun outputConfiguration(path: String): String {
     val libraryExport = path
         .split(".")
         .map { "'$it'" }
         .joinToString(", ")
 
+    // language=JavaScript
     return """
             config.output = config.output || {}
             config.output.libraryTarget = 'umd'
