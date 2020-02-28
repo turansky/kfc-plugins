63647f115316a6a148e254ed92a54a8b2270a4b3
Victor Turansky <victor.turansky@gmail.com>
2020-02-28 11:49:54 +0300

    Serializable input property

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebComponent.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebComponent.kt
@@ -1,11 +1,13 @@
 package com.github.turansky.kfc.gradle.plugin
 
+import java.io.Serializable
+
 data class WebComponent(
     val id: String,
     val properties: List<Property>,
     val events: List<String>,
     val source: String
-) {
+) : Serializable {
     val sourceRoot: String = source.substringBeforeLast(".")
 
     data class Property(
