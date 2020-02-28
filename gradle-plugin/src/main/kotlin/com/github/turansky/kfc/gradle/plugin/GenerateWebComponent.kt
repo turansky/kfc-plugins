0c1eec9e22ce56676cbe0fe3c2c4a99f5af510c2
Victor Turansky <victor.turansky@gmail.com>
2020-02-28 04:35:38 +0300

    Simple GenerateWebComponent task

---
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/GenerateWebComponent.kt
@@ -0,0 +1,21 @@
+package com.github.turansky.kfc.gradle.plugin
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+open class GenerateWebComponent : DefaultTask() {
+    @get:Input
+    var component: WebComponent? = null
+
+    val entry: File
+        get() = temporaryDir.resolve("index.js")
+
+    @TaskAction
+    private fun generate() {
+        val component = checkNotNull(component)
+        temporaryDir.resolve("index.js")
+            .writeText(component.toString())
+    }
+}
