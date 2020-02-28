9c05dd4860381ebced585eae0900c4febbe856e1
Victor Turansky <victor.turansky@gmail.com>
2020-02-28 04:26:54 +0300

    API for WebComponentExtension

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebComponentExtension.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebComponentExtension.kt
@@ -1,6 +1,41 @@
 package com.github.turansky.kfc.gradle.plugin
 
+import com.github.turansky.kfc.gradle.plugin.WebComponent.Property
+import com.github.turansky.kfc.gradle.plugin.WebComponent.Property.Type
+import com.github.turansky.kfc.gradle.plugin.WebComponent.Property.Type.*
+
 open class WebComponentExtension {
     lateinit var id: String
     lateinit var source: String
+
+    private val _properties: MutableList<Property> = mutableListOf()
+    private val _events: MutableList<String> = mutableListOf()
+
+    fun property(name: String) {
+        addProperty(name, RW)
+    }
+
+    fun getter(name: String) {
+        addProperty(name, RO)
+    }
+
+    fun setter(name: String) {
+        addProperty(name, WO)
+    }
+
+    private fun addProperty(name: String, type: Type) {
+        _properties += Property(name, type)
+    }
+
+    fun events(vararg events: String) {
+        _events += events
+    }
+
+    fun build(): WebComponent =
+        WebComponent(
+            id = id,
+            properties = _properties.toList(),
+            events = _events.toList(),
+            source = source
+        )
 }
