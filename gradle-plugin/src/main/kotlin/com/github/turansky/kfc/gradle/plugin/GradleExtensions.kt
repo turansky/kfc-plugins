9875db8e97b3221400e24b4535055fab3b665d99
Victor Turansky <victor.turansky@gmail.com>
2020-02-29 15:05:06 +0300

    Update issue link

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/GradleExtensions.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/GradleExtensions.kt
@@ -3,8 +3,8 @@ package com.github.turansky.kfc.gradle.plugin
 import org.gradle.api.DomainObjectCollection
 import org.gradle.kotlin.dsl.withType
 
-// https://github.com/gradle/gradle/issues/12266
-inline fun <reified S : Any> DomainObjectCollection<in S>.configureEach(
+// https://github.com/gradle/gradle/issues/9832
+internal inline fun <reified S : Any> DomainObjectCollection<in S>.configureEach(
     noinline action: S.() -> Unit
 ) {
     withType().configureEach(action)
