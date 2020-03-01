371eb2aba81c6894239edc787cc096124987fab2
Victor Turansky <victor.turansky@gmail.com>
2020-03-01 04:42:15 +0300

    Simplify yFiles externals logic

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/YFilesPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/YFilesPlugin.kt
@@ -2,50 +2,38 @@ package com.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.withType
 
 // language=JavaScript
 private val EXTERNALS = """
-config.externals = [
-  /^yfiles${'$'}/i,
-  /^yfiles\/.+${'$'}/i
-]
+    if (config.mode !== 'production') {
+      return
+    }
+    
+    config.externals = [
+      /^yfiles${'$'}/i,
+      /^yfiles\/.+${'$'}/i
+    ]
 """.trimIndent()
 
 // language=JavaScript
 private val RULES = """
-  config.module.rules.push(
-    {
-      test: /\.css${'$'}/,
-      use: ['style-loader', 'css-loader']
-    },
-    {
-      test: /\.svg${'$'}/,
-      use: 'svg-inline-loader'
-    }
-  )
+    config.module.rules.push(
+      {
+        test: /\.css${'$'}/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.svg${'$'}/,
+        use: 'svg-inline-loader'
+      }
+    )
 """.trimIndent()
 
 class YFilesPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
-        plugins.withType<LibraryPlugin> {
-            tasks.configureEach<PatchWebpackConfig> {
-                patch("rules", RULES)
-            }
-        }
-
-        plugins.withType<ComponentPlugin> {
-            tasks.configureEach<PatchWebpackConfig> {
-                patch("externals", EXTERNALS)
-                patch("rules", RULES)
-            }
-        }
-
-        plugins.withType<WebComponentPlugin> {
-            tasks.configureEach<PatchWebpackConfig> {
-                patch("externals", EXTERNALS)
-                patch("rules", RULES)
-            }
+        tasks.configureEach<PatchWebpackConfig> {
+            patch("externals", EXTERNALS)
+            patch("rules", RULES)
         }
     }
 }
