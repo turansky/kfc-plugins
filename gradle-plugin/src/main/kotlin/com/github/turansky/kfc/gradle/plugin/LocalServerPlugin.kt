1e0b299b94d0755018dc305f959360d69db287a4
Victor Turansky <victor.turansky@gmail.com>
2020-03-01 03:17:14 +0300

    Add local server plugin

---
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/LocalServerPlugin.kt
@@ -0,0 +1,62 @@
+package com.github.turansky.kfc.gradle.plugin
+
+import com.github.turansky.kfc.gradle.plugin.JsTarget.COMMONJS
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+import org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
+import org.jetbrains.kotlin.gradle.tasks.KotlinJsDce
+
+private val RUN_TASKS = setOf(
+    "browserDevelopmentRun"
+)
+
+open class LocalServerExtension {
+    var root: String? = null
+}
+
+class LocalServerPlugin : Plugin<Project> {
+    override fun apply(target: Project): Unit = with(target) {
+        plugins.withType<KotlinJsPluginWrapper> {
+            plugins.apply(WebpackPlugin::class)
+
+            val extension = extensions.create<LocalServerExtension>("localServer")
+
+            tasks {
+                configureEach<KotlinJsCompile> {
+                    kotlinOptions {
+                        moduleKind = COMMONJS
+                    }
+                }
+
+                configureEach<KotlinJsDce> {
+                    enabled = false
+                }
+
+                configureEach<KotlinWebpack> {
+                    if (name !in RUN_TASKS) {
+                        enabled = false
+                    }
+
+                    outputFileName = LOCAL_SERVER_JS
+                    sourceMaps = false
+                }
+            }
+
+            afterEvaluate {
+                val localServerRoot = extension.root
+                    ?: return@afterEvaluate
+
+                tasks.configureEach<PatchWebpackConfig> {
+                    patch("output", outputConfiguration(localServerRoot))
+                }
+            }
+        }
+    }
+}
+
--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Output.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Output.kt
@@ -2,3 +2,4 @@ package com.github.turansky.kfc.gradle.plugin
 
 internal const val LIBRARY_JS: String = "library.js"
 internal const val COMPONENT_JS: String = "component.js"
+internal const val LOCAL_SERVER_JS: String = "local-server.js"
