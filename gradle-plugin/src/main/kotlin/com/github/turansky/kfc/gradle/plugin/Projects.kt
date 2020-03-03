caceef53d81b79137ed85e0f5176b35a5090b64f
Victor Turansky <victor.turansky@gmail.com>
2020-03-03 10:46:17 +0300

    Calculate js package dir

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Projects.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Projects.kt
@@ -3,11 +3,17 @@ package com.github.turansky.kfc.gradle.plugin
 import org.gradle.api.Project
 import org.gradle.api.internal.artifacts.dependencies.DefaultProjectDependency
 import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependency
+import java.io.File
 
 private val IMPLEMENTATION = "implementation"
 
 internal val Project.jsProjectId: String
     get() = "${rootProject.name}-$name"
+
+internal val Project.jsPackageDir: File
+    get() = rootProject.buildDir
+        .resolve("packages")
+        .resolve(jsProjectId)
 
 // TODO: optimize calculation
 internal fun Project.relatedProjects(): Set<Project> {
