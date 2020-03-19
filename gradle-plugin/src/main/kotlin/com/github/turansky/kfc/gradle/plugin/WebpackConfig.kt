fe8160c2e84e02f9235a31814cce32a6b5c2e2ff
Victor Turansky <victor.turansky@gmail.com>
2020-03-20 02:13:11 +0300

    Add multi output configuration

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebpackConfig.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebpackConfig.kt
@@ -19,6 +19,36 @@ internal fun outputConfiguration(path: String): String =
         delete config.output.library
     """.trimIndent()
 
+internal fun outputConfiguration(outputs: List<WebpackOutput>): String {
+    val configs = outputs
+        .joinToString(",\n\n")
+        { outputConfiguration(it) }
+
+    // language=JavaScript
+    return """
+        // multi output
+        
+        config.output = config.output || {}
+        delete config.output.library
+
+        config = [
+          $configs
+        ]
+    """.trimIndent()
+}
+
+private fun outputConfiguration(output: WebpackOutput): String =
+    // language=JavaScript
+    """
+          Object.assign({}, config, {
+            name: ${output.name}
+            output: {
+              filename: "[name].js",
+              libraryExport: ${libraryExport(output.root)}
+            }
+          })
+    """.trimIndent()
+
 internal fun entryConfiguration(
     output: Output? = null,
     entry: File
