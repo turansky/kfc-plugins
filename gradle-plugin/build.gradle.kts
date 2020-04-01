f0d54745c8b5f8a4bab4c97febd46f57c8fb6e06
Victor Turansky <victor.turansky@gmail.com>
2020-04-01 23:56:14 +0300

    Version - 0.7.5

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     `kotlin-dsl`
 
     id("com.gradle.plugin-publish") version "0.11.0"
-    id("com.github.turansky.kfc.plugin-publish") version "0.7.0"
+    id("com.github.turansky.kfc.plugin-publish") version "0.7.5"
 
     kotlin("jvm") version "1.3.71"
 }
