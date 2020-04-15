37408ada26dbc1ea79cfa3ea465d37142be3db29
Victor Turansky <victor.turansky@gmail.com>
2020-04-15 20:34:35 +0300

    Add plugin enum

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,3 +1,7 @@
+import Build_gradle.KfcPlugin.*
+import com.gradle.publish.PluginBundleExtension
+import com.gradle.publish.PluginConfig
+
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
@@ -25,12 +29,6 @@ dependencies {
 val REPO_URL = "https://github.com/turansky/kfc-plugins"
 val PROJECT_VERSION = project.version.toString()
 
-fun kfc(name: String): String =
-    "com.github.turansky.kfc.$name"
-
-fun pluginClass(className: String): String =
-    "com.github.turansky.kfc.gradle.plugin.$className"
-
 fun tags(vararg pluginTags: String): List<String> =
     listOf(
         "kotlin",
@@ -38,77 +36,46 @@ fun tags(vararg pluginTags: String): List<String> =
         "javascript"
     ) + pluginTags
 
-val ROOT = "root"
-val VERSION = "version"
+enum class KfcPlugin(className: String) {
+    ROOT("RootPlugin"),
+    VERSION("VersionPlugin"),
 
-val NPM_BUILD = "npm-build"
-val MAVEN_PUBLISH = "maven-publish"
+    NPM_BUILD("NpmBuildPlugin"),
+    MAVEN_PUBLISH("MavenPublishPlugin"),
 
-val WEBPACK = "webpack"
-val LIBRARY = "library"
-val COMPONENT = "component"
-val WEB_COMPONENT = "webcomponent"
-val YFILES = "yfiles"
-val DEV_SERVER = "dev-server"
+    WEBPACK("WebpackPlugin"),
+    LIBRARY("LibraryPlugin"),
+    COMPONENT("ComponentPlugin"),
+    WEBCOMPONENT("WebComponentPlugin"),
+    YFILES("YFilesPlugin"),
+    DEV_SERVER("DevServerPlugin"),
 
-val PLUGIN_PUBLISH = "plugin-publish"
+    PLUGIN_PUBLISH("PluginPublishPlugin");
+
+    val pluginName: String = name.toLowerCase().replace("_", "-")
+
+    val id: String = "com.github.turansky.kfc.$pluginName"
+    val implementationClass: String = "com.github.turansky.kfc.gradle.plugin.$className"
+}
 
 gradlePlugin {
     plugins {
-        create(ROOT) {
-            id = kfc(ROOT)
-            implementationClass = pluginClass("RootPlugin")
+        for (kfcPlugin in values()) {
+            create(kfcPlugin.pluginName) {
+                id = kfcPlugin.id
+                implementationClass = kfcPlugin.implementationClass
+            }
         }
+    }
+}
 
-        create(VERSION) {
-            id = kfc(VERSION)
-            implementationClass = pluginClass("VersionPlugin")
-        }
-
-        create(NPM_BUILD) {
-            id = kfc(NPM_BUILD)
-            implementationClass = pluginClass("NpmBuildPlugin")
-        }
-
-        create(MAVEN_PUBLISH) {
-            id = kfc(MAVEN_PUBLISH)
-            implementationClass = pluginClass("MavenPublishPlugin")
-        }
-
-        create(WEBPACK) {
-            id = kfc(WEBPACK)
-            implementationClass = pluginClass("WebpackPlugin")
-        }
-
-        create(LIBRARY) {
-            id = kfc(LIBRARY)
-            implementationClass = pluginClass("LibraryPlugin")
-        }
-
-        create(COMPONENT) {
-            id = kfc(COMPONENT)
-            implementationClass = pluginClass("ComponentPlugin")
-        }
-
-        create(WEB_COMPONENT) {
-            id = kfc(WEB_COMPONENT)
-            implementationClass = pluginClass("WebComponentPlugin")
-        }
-
-        create(YFILES) {
-            id = kfc(YFILES)
-            implementationClass = pluginClass("YFilesPlugin")
-        }
-
-        create(DEV_SERVER) {
-            id = kfc(DEV_SERVER)
-            implementationClass = pluginClass("DevServerPlugin")
-        }
-
-        create(PLUGIN_PUBLISH) {
-            id = kfc(PLUGIN_PUBLISH)
-            implementationClass = pluginClass("PluginPublishPlugin")
-        }
+fun PluginBundleExtension.plugin(
+    kfcPlugin: KfcPlugin,
+    action: PluginConfig.() -> Unit
+) {
+    plugins.getByName(kfcPlugin.pluginName) {
+        version = PROJECT_VERSION
+        action()
     }
 }
 
@@ -116,81 +83,70 @@ pluginBundle {
     website = REPO_URL
     vcsUrl = REPO_URL
 
-    plugins.getByName(ROOT) {
+    plugin(ROOT) {
         displayName = "Root plugin"
         description = "Configure Kotlin/JS plugin in root project"
         tags = tags("root")
-        version = PROJECT_VERSION
     }
 
-    plugins.getByName(VERSION) {
+    plugin(VERSION) {
         displayName = "Version plugin"
         description = "Provide version tasks in root project"
         tags = tags("version")
-        version = PROJECT_VERSION
     }
 
-    plugins.getByName(NPM_BUILD) {
+    plugin(NPM_BUILD) {
         displayName = "npm build plugin"
         description = "Predefined npm tasks for Kotlin/JS"
         tags = tags("npm")
-        version = PROJECT_VERSION
     }
 
-    plugins.getByName(MAVEN_PUBLISH) {
+    plugin(MAVEN_PUBLISH) {
         displayName = "Maven publish plugin"
         description = "Predefined maven publications for Kotlin"
         tags = tags("maven", "publish")
-        version = PROJECT_VERSION
     }
 
-    plugins.getByName(WEBPACK) {
+    plugin(WEBPACK) {
         displayName = "Kotlin/JS webpack plugin"
         description = "Webpack configuration for Kotlin/JS"
         tags = tags("webpack", "config")
-        version = PROJECT_VERSION
     }
 
-    plugins.getByName(LIBRARY) {
+    plugin(LIBRARY) {
         displayName = "Kotlin library plugin"
         description = "Optimize Kotlin library configuration"
         tags = tags("library")
-        version = PROJECT_VERSION
     }
 
-    plugins.getByName(COMPONENT) {
+    plugin(COMPONENT) {
         displayName = "Kotlin/JS component plugin"
         description = "Optimize Kotlin/JS component configuration"
         tags = tags("component")
-        version = PROJECT_VERSION
     }
 
-    plugins.getByName(WEB_COMPONENT) {
+    plugin(WEBCOMPONENT) {
         displayName = "Kotlin/JS web component plugin"
         description = "Web component adapter for Kotlin/JS"
         tags = tags("webcomponent")
-        version = PROJECT_VERSION
     }
 
-    plugins.getByName(YFILES) {
+    plugin(YFILES) {
         displayName = "yFiles configuration plugin"
         description = "Provide configuration for yFiles"
         tags = tags("yfiles")
-        version = PROJECT_VERSION
     }
 
-    plugins.getByName(DEV_SERVER) {
+    plugin(DEV_SERVER) {
         displayName = "Development server plugin"
         description = "Testing server for Kotlin/JS"
         tags = tags("dev server", "dev testing")
-        version = PROJECT_VERSION
     }
 
-    plugins.getByName(PLUGIN_PUBLISH) {
+    plugin(PLUGIN_PUBLISH) {
         displayName = "Plugin publish plugin"
         description = "Provide publish tasks for gradle plugin project"
         tags = tags("gradle", "publish")
-        version = PROJECT_VERSION
     }
 }
 
