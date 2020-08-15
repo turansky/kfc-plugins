6af17cac96264863630d73adbe9bfe72cd43cdb1
Victor Turansky <victor.turansky@gmail.com>
2020-08-15 16:28:45 +0300

    Remove redundant dependencies

--- examples/webpack-plus-ktor/entity/build.gradle.kts
+++ examples/webpack-plus-ktor/entity/build.gradle.kts
@@ -1,25 +1,3 @@
 plugins {
     id("com.github.turansky.kfc.multiplatform")
 }
-
-kotlin {
-    sourceSets {
-        commonMain {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-            }
-        }
-
-        jvmMain {
-            dependencies {
-                implementation(kotlin("stdlib-jdk8"))
-            }
-        }
-
-        jsMain {
-            dependencies {
-                implementation(kotlin("stdlib-js"))
-            }
-        }
-    }
-}
--- examples/webpack-plus-ktor/ktor-server/build.gradle.kts
+++ examples/webpack-plus-ktor/ktor-server/build.gradle.kts
@@ -12,8 +12,6 @@ val ktorVersion = "1.3.2"
 val logbackVersion = "1.2.3"
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-html-builder:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
