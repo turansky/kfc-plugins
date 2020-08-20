029bead46bbf8c45759ad3aad779ed429cc128c2
Victor Turansky <victor.turansky@gmail.com>
2020-08-20 04:47:48 +0300

    Remove NPM dependency util method

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Projects.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Projects.kt
@@ -2,7 +2,6 @@ package com.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Project
 import org.gradle.api.internal.artifacts.dependencies.DefaultProjectDependency
-import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependency
 
 private val IMPLEMENTATION = "implementation"
 
@@ -22,13 +21,4 @@ internal fun Project.relatedProjects(): Set<Project> {
         .flatMap { sequenceOf(it) + it.relatedProjects() }
         .plus(this)
         .toSet()
-}
-
-internal fun Project.nmpDependencies(): List<NpmDependency> {
-    val configuration = configurations.findByName(IMPLEMENTATION)
-        ?: return emptyList()
-
-    return configuration
-        .allDependencies
-        .filterIsInstance<NpmDependency>()
 }
