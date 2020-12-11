e0bdb84aae97f9570c43cdee0b37f593b1aa4ffa
Victor Turansky <victor.turansky@gmail.com>
2020-12-11 09:42:37 +0300

    Add `CustomElement` base class

---
+++ examples/webpack-plus-ktor/view/src/main/kotlin/org/w3c/dom/CustomElement.kt
@@ -0,0 +1,40 @@
+package org.w3c.dom
+
+import org.w3c.dom.css.CSSStyleDeclaration
+
+@JsName("HTMLElement")
+external class CustomElement : HTMLElement {
+    override val childElementCount: Int
+    override val children: HTMLCollection
+    override var contentEditable: String
+    override val isContentEditable: Boolean
+    override val style: CSSStyleDeclaration
+
+    override fun after(vararg nodes: dynamic)
+    override fun append(vararg nodes: dynamic)
+    override fun before(vararg nodes: dynamic)
+    override fun convertPointFromNode(
+        point: DOMPointInit,
+        from: dynamic,
+        options: ConvertCoordinateOptions,
+    ): DOMPoint
+
+    override fun convertQuadFromNode(
+        quad: dynamic,
+        from: dynamic,
+        options: ConvertCoordinateOptions,
+    ): DOMQuad
+
+    override fun convertRectFromNode(
+        rect: DOMRectReadOnly,
+        from: dynamic,
+        options: ConvertCoordinateOptions,
+    ): DOMQuad
+
+    override fun getBoxQuads(options: BoxQuadOptions): Array<DOMQuad>
+    override fun prepend(vararg nodes: dynamic)
+    override fun querySelector(selectors: String): Element?
+    override fun querySelectorAll(selectors: String): NodeList
+    override fun remove()
+    override fun replaceWith(vararg nodes: dynamic)
+}
