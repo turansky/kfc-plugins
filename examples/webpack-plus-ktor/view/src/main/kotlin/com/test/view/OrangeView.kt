809068ab8c42c3310b863a2f75ce6a2f4d87b00b
Victor Turansky <victor.turansky@gmail.com>
2020-12-12 10:11:10 +0300

    Update test custom elements

--- examples/webpack-plus-ktor/view/src/main/kotlin/com/test/view/OrangeView.kt
+++ examples/webpack-plus-ktor/view/src/main/kotlin/com/test/view/OrangeView.kt
@@ -1,13 +1,13 @@
 package com.test.view
 
+import org.w3c.dom.CLOSED
 import org.w3c.dom.CustomElement
-import org.w3c.dom.OPEN
 import org.w3c.dom.ShadowRootInit
 import org.w3c.dom.ShadowRootMode
 
 class OrangeView : CustomElement() {
     init {
-        val shadow = attachShadow(ShadowRootInit(ShadowRootMode.OPEN))
+        val shadow = attachShadow(ShadowRootInit(ShadowRootMode.CLOSED))
         shadow.appendChild(div("orange"))
     }
 }
