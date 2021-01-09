b34b65dbfed10d141fb2d77f49a7758ad586a906
Victor Turansky <victor.turansky@gmail.com>
2021-01-09 05:36:28 +0300

    Update plugin

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     `kotlin-dsl`
 
     id("com.gradle.plugin-publish") version "0.12.0"
-    id("com.github.turansky.kfc.plugin-publish") version "2.2.0"
+    id("com.github.turansky.kfc.plugin-publish") version "2.3.0"
 }
 
 repositories {
