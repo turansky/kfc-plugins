bc548dbc8dd5cf1ebcaf78335f32b6f5561191c6
Victor Turansky <victor.turansky@gmail.com>
2021-02-26 22:39:00 +0300

    Web Worker. Simple local server configuration

--- examples/web-worker/local-server/build.gradle.kts
+++ examples/web-worker/local-server/build.gradle.kts
@@ -1,13 +1,45 @@
 plugins {
     kotlin("js")
+    id("com.github.turansky.kfc.webpack")
 }
 
+val MODULE_NAME = "ww-local-server"
+
 kotlin.js {
-    moduleName = "ww-local-server"
+    moduleName = MODULE_NAME
 
     browser()
     useCommonJs()
     binaries.executable()
+}
+
+tasks.patchWebpackConfig {
+    val runDir = project.rootProject.buildDir
+        .resolve("js/packages")
+        .resolve(MODULE_NAME)
+        .resolve("kotlin-dce-dev")
+
+    val view = runDir.resolve("ww-view.js")
+    val worker = runDir.resolve("ww-worker.js")
+
+    // language=JavaScript
+    patch(
+        """
+        if (config.mode !== 'development') {
+            return
+        }
+        
+        delete config.entry.main
+
+        const output = config.output
+        output.filename = '[name].js'
+        output.libraryTarget = 'umd'
+        delete output.library 
+        
+        config.entry.view = '${view.absolutePath}'
+        config.entry.worker = '${worker.absolutePath}'
+    """
+    )
 }
 
 dependencies {
