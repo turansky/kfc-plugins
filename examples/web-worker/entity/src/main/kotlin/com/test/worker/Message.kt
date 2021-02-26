2df1c2a853e860ed4bd19c14ec6b0426e9a9949f
Victor Turansky <victor.turansky@gmail.com>
2021-02-27 02:09:55 +0300

    Web Worker. Add `Message` class

---
+++ examples/web-worker/entity/src/main/kotlin/com/test/worker/Message.kt
@@ -0,0 +1,27 @@
+package com.test.worker
+
+external interface Message {
+    val type: String
+    val data: Any
+}
+
+object MessageType {
+    val COUNT: String = "count"
+    val INFO: String = "info"
+}
+
+private fun Message(
+    type: String,
+    data: Any,
+): Message {
+    val m = js("({})")
+    m.type = type
+    m.data = data
+    return m
+}
+
+fun Message(count: Int): Message =
+    Message(MessageType.COUNT, count)
+
+fun Message(message: String): Message =
+    Message(MessageType.INFO, message)
