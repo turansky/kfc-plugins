7b744900ce42417a5c31d3e16b050c5b459851d9
Victor Turansky <victor.turansky@gmail.com>
2021-03-05 08:59:08 +0300

    Add module name configuration

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/KotlinJs.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/KotlinJs.kt
@@ -3,7 +3,27 @@ package com.github.turansky.kfc.gradle.plugin
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsProjectExtension
 
+private val MODULE_NAME = StringProperty("kfc.module.name")
+private val OUTPUT_NAME = StringProperty("kfc.output.name")
+
 private const val BUILD_DISTRIBUTION = "kotlin.js.generate.executable.default"
+
+private fun Project.getModuleName(): String {
+    propertyOrNull(MODULE_NAME)
+        ?.let { return it }
+
+    return when (this) {
+        rootProject -> rootProject.name
+        else -> "${rootProject.name}-$name}"
+    }
+}
+
+private fun Project.getOutputFileName(): String {
+    val name = propertyOrNull(OUTPUT_NAME)
+        ?: getModuleName()
+
+    return "$name.js"
+}
 
 internal fun Project.applyKotlinJsPlugin(
     distribution: Boolean = false
@@ -12,11 +32,16 @@ internal fun Project.applyKotlinJsPlugin(
 
     plugins.apply(KotlinPlugin.JS)
 
+    val fileName = getOutputFileName()
+
     extensions.configure<KotlinJsProjectExtension>("kotlin") {
         js {
+            moduleName = getModuleName()
+
             browser {
                 webpackTask {
                     output.library = null
+                    outputFileName = fileName
                 }
             }
 
