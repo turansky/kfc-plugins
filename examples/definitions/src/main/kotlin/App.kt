060bf6c66a1bc0e9609f7b3623959c1a57b6b466
Victor Turansky <victor.turansky@gmail.com>
2021-03-14 01:36:41 +0300

    Update `definitions` example

--- examples/definitions/src/main/kotlin/App.kt
+++ examples/definitions/src/main/kotlin/App.kt
@@ -1,6 +1,9 @@
+import kotlinx.browser.document
 import kotlinx.browser.window
+import org.w3c.dom.HTMLElement
 import org.w3c.dom.url.URL
 import org.w3c.fetch.RequestInit
+import kotlin.js.Date
 import kotlin.js.Promise
 
 @JsExport
@@ -9,3 +12,14 @@ fun load(url: URL): Promise<String> =
     window.fetch("https://httpbin.org/get", RequestInit(headers = js("({})")))
         .then { response -> response.json() }
         .then { it.unsafeCast<String>() }
+
+@JsExport
+@OptIn(ExperimentalJsExport::class)
+fun embed(element: HTMLElement) {
+    document.body!!.appendChild(element)
+}
+
+@JsExport
+@OptIn(ExperimentalJsExport::class)
+fun now(): Date =
+    Date()
