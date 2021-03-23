14b21ed6d613f8b00beeb045bc04a5d7d8e28ca4
Victor Turansky <victor.turansky@gmail.com>
2021-03-23 05:32:59 +0300

    Update source map logic

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/SourceMapsPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/SourceMapsPlugin.kt
@@ -2,22 +2,22 @@ package com.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 
-private val SOURCE_MAPS = BooleanProperty("kfc.source.maps")
+private val SOURCE_MAPS = BooleanProperty("kfc.source.maps", true)
 
 internal class SourceMapsPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         if (!property(SOURCE_MAPS)) {
-            tasks.withType<KotlinJsCompile>().configureEach {
+            tasks.configureEach<KotlinJsCompile> {
                 kotlinOptions {
                     sourceMap = false
+                    sourceMapEmbedSources = "never"
                 }
             }
 
-            tasks.withType<KotlinWebpack>().configureEach {
+            tasks.configureEach<KotlinWebpack> {
                 sourceMaps = false
             }
         }
