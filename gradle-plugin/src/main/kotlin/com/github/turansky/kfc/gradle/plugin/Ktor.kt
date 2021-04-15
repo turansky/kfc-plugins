ef72e51bdba0e561b2a97dec9aece6219525384a
Victor Turansky <victor.turansky@gmail.com>
2021-04-16 02:56:15 +0300

    Webpack 5 support for Ktor patch

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Ktor.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Ktor.kt
@@ -1,31 +1,19 @@
 package com.github.turansky.kfc.gradle.plugin
 
-private const val NULL_ALIAS = "NULL_ALIAS"
-
-// language=JavaScript
-private const val NULL_ALIAS_CONTENT = "export const KY = 13"
-
 internal fun PatchWebpackConfig.patchKtorDependencies() {
     val ignoreFile = project.rootDir.resolve(".ktorignore")
         .takeIf { it.exists() }
         ?: return
-
-    val nullAlias = temporaryDir.resolve("null-alias.js")
-
-    doFirst {
-        nullAlias.writeText(NULL_ALIAS_CONTENT)
-    }
 
     val aliases = ignoreFile
         .readLines()
         .asSequence()
         .map { it.trim() }
         .filter { it.isNotEmpty() }
-        .joinToString(",\n") { "'$it': $NULL_ALIAS" }
+        .joinToString(",\n") { "'$it': false" }
 
     patch(
         """
-    const $NULL_ALIAS = '${nullAlias.absolutePath}'    
     config.resolve.alias = {
         $aliases
     }    
--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Momentjs.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/Momentjs.kt
@@ -3,7 +3,7 @@ package com.github.turansky.kfc.gradle.plugin
 internal object Momentjs {
     val IGNORE_LOCALES_FLAG: BooleanProperty = BooleanProperty("kfc.ignore.momentjs.locales")
 
-    // language=Kotlin
+    // language=JavaScript
     val IGNORE_LOCALES_PATCH: String = """
        const { IgnorePlugin } = require('webpack')
        config.plugins.push(
