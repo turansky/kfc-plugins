b749cc3ea89ef8b88a353be8325e036f9d0543f1
Victor Turansky <victor.turansky@gmail.com>
2021-04-29 06:10:54 +0300

    Application. Replace runtime dependencies for webpack

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ApplicationPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ApplicationPlugin.kt
@@ -2,6 +2,15 @@ package com.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
+import org.gradle.kotlin.dsl.getValue
+import org.gradle.kotlin.dsl.named
+import org.gradle.kotlin.dsl.provideDelegate
+import org.gradle.kotlin.dsl.registering
+import org.jetbrains.kotlin.gradle.tasks.KotlinJsDce
+
+private const val BPW = "browserProductionWebpack"
+private const val BDW = "browserDevelopmentWebpack"
 
 class ApplicationPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
@@ -19,6 +28,18 @@ class ApplicationPlugin : Plugin<Project> {
     }
 
     private fun Project.applyLegacy() {
-        // Implement
+        val replaceWorker by tasks.registering(Copy::class) {
+            for (runtimeProject in project.relatedRuntimeProjects())
+                from(runtimeProject.tasks.getByName(BPW))
+
+            val processDceKotlinJs = tasks.named<KotlinJsDce>("processDceKotlinJs")
+            into(processDceKotlinJs.get().destinationDirectory)
+
+            dependsOn(processDceKotlinJs)
+        }
+
+        tasks.named(BPW) {
+            dependsOn(replaceWorker)
+        }
     }
 }
