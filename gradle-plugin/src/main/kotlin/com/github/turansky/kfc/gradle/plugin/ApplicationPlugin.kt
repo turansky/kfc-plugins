3866a07140a8a88a73bea5375c55a3fe98636d96
Victor Turansky <victor.turansky@gmail.com>
2021-04-29 05:31:50 +0300

    Application. Add per compiler `apply` methods

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ApplicationPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ApplicationPlugin.kt
@@ -6,5 +6,19 @@ import org.gradle.api.Project
 class ApplicationPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         applyKotlinJsPlugin(distribution = true)
+
+        if (jsIrCompiler) {
+            applyIr()
+        } else {
+            applyLegacy()
+        }
+    }
+
+    private fun Project.applyIr() {
+        // Implement
+    }
+
+    private fun Project.applyLegacy() {
+        // Implement
     }
 }
