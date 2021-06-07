6637cd3fc18578f5dc7c39e286e1ac27af097584
Victor Turansky <victor.turansky@gmail.com>
2021-06-07 03:08:04 +0300

    Suppress warnings

--- examples/definitions/src/main/kotlin/App.kt
+++ examples/definitions/src/main/kotlin/App.kt
@@ -1,14 +1,13 @@
 import kotlinx.browser.document
 import kotlinx.browser.window
 import org.w3c.dom.HTMLElement
-import org.w3c.dom.url.URL
 import org.w3c.fetch.RequestInit
 import kotlin.js.Date
 import kotlin.js.Promise
 
 @JsExport
 @ExperimentalJsExport
-fun load(url: URL): Promise<String> =
+fun load(): Promise<String> =
     window.fetch("https://httpbin.org/get", RequestInit(headers = js("({})")))
         .then { response -> response.json() }
         .then { it.unsafeCast<String>() }
