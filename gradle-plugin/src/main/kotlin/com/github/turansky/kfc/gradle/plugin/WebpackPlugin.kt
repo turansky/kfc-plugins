a15ee1c7b63472fd07c73f8c62ddd703adada88e
Victor Turansky <victor.turansky@gmail.com>
2021-09-02 21:59:57 +0300

    Webpack Dev Server `4.1.0`

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebpackPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/WebpackPlugin.kt
@@ -12,9 +12,9 @@ private class WebpackRootPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         plugins.withType<NodeJsRootPlugin> {
             the<NodeJsRootExtension>().versions.apply {
-                webpack.version = "5.51.1"
+                webpack.version = "5.51.2"
                 webpackCli.version = "4.8.0"
-                webpackDevServer.version = "4.0.0"
+                webpackDevServer.version = "4.1.0"
             }
         }
     }
