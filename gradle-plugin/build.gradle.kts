d9b8d7bc8df42f9d0cc3166c1c325eaa6cce861c
Victor Turansky <victor.turansky@gmail.com>
2021-11-07 01:44:16 +0300

    Version - `4.43.0`

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     `kotlin-dsl`
 
     id("com.gradle.plugin-publish") version "0.16.0"
-    id("com.github.turansky.kfc.plugin-publish") version "4.42.0"
+    id("com.github.turansky.kfc.plugin-publish") version "4.43.0"
 }
 
 dependencies {
