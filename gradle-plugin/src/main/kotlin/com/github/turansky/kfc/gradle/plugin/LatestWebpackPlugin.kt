565dd88454b8d93ce3c771540535d14b6f8906b0
Victor Turansky <victor.turansky@gmail.com>
2021-11-15 12:58:57 +0300

    Webpack Dev Server `4.5.0`

--- gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/LatestWebpackPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/LatestWebpackPlugin.kt
@@ -16,9 +16,9 @@ class LatestWebpackPlugin : Plugin<Project> {
 private fun Project.configureLatestWebpack() {
     plugins.withType<NodeJsRootPlugin> {
         the<NodeJsRootExtension>().versions.apply {
-            webpack.version = "5.63.0"
+            webpack.version = "5.64.0"
             webpackCli.version = "4.9.1"
-            webpackDevServer.version = "4.4.0"
+            webpackDevServer.version = "4.5.0"
         }
     }
 }
