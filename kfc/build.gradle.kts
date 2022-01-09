e10db87034aa0cbd91c34f143b64f034d06ba957
Victor Turansky <victor.turansky@gmail.com>
2022-01-09 06:08:57 +0300

    Configure MC publication

--- kfc/build.gradle.kts
+++ kfc/build.gradle.kts
@@ -1,5 +1,15 @@
 plugins {
     kotlin("js") apply false
+    id("io.github.gradle-nexus.publish-plugin")
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
 }
 
 tasks.wrapper {
