b985010e24893190f8fe1fc7a30fd855e528a5f9
Victor Turansky <victor.turansky@gmail.com>
2022-01-09 05:53:55 +0300

    Add wrappers dependency

--- kfc/kfc-react/build.gradle.kts
+++ kfc/kfc-react/build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     id("com.github.turansky.kfc.library")
     id("com.github.turansky.kfc.maven-central-publish")
 }
+
+val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
+
+dependencies {
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-core")
+}
