4a4d5eff52a7315e8ee4094fdb959bf4e00f77dc
Victor Turansky <victor.turansky@gmail.com>
2022-01-10 05:47:45 +0300

    Implement React component import

--- kfc/kfc-gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ReactPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/ReactPlugin.kt
@@ -5,6 +5,8 @@ import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 import java.io.File
 
+private val REACT_COMPONENT_IMPORT = Regex("""__react_component_import__\((.+?)\)""")
+
 class ReactPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         tasks.configureEach<KotlinJsCompile> {
@@ -21,11 +23,19 @@ private fun addLazySupport(
     if (file.extension != "js")
         return
 
+    val appDir = file.parentFile
+    val appName = file.name.substringBeforeLast(".")
+
     val content = file.readText()
 
     val (bodySource, librariesSource) = content.split("\n  main();\n  return _;\n}(module.exports, ")
 
     val body = bodySource.substringAfter("{\n")
+        .replace(REACT_COMPONENT_IMPORT) {
+            val componentName = it.groups[1]!!.value
+            "import('./${appName}__$componentName')"
+        }
+
     val libraryIds = bodySource
         .substringBefore("{\n")
         .removeSurrounding("(function (_, ", ") ")
@@ -38,5 +48,36 @@ private fun addLazySupport(
         .mapIndexed { index, library -> "import ${libraryIds[index]} from $library;" }
         .joinToString("\n")
 
-    file.writeText("$imports\n\n$body\n\nmain();")
+    val componentNames = REACT_COMPONENT_IMPORT
+        .findAll(content)
+        .map { it.groups[1]!!.value }
+
+    val exports = componentNames.asSequence()
+        .plus("main")
+        .joinToString("\n", "export {\n", "\n};") {
+            "  $it,"
+        }
+
+    val mainLibName = "${appName}__lib"
+    appDir.resolve("$mainLibName.js")
+        .writeText("$imports\n\n$body\n\n$exports")
+
+    for (componentName in componentNames) {
+        appDir.resolve("${appName}__$componentName.js")
+            .writeText(
+                """
+                import { $componentName } from './$mainLibName'
+            
+                export default $componentName
+                """.trimIndent()
+            )
+    }
+
+    file.writeText(
+        """
+        import { main } from './$mainLibName'
+        
+        main()
+        """.trimIndent()
+    )
 }
