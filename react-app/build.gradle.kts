ace4e3ff4fd7971d7a34d272a85c4527e7deb522
Victor Turansky <victor.turansky@gmail.com>
2022-01-10 03:25:57 +0300

    Fix dependencies

--- react-app/build.gradle.kts
+++ react-app/build.gradle.kts
@@ -1,11 +1,18 @@
 plugins {
-    id("com.github.turansky.kfc.application")
+    kotlin("js")
+}
+
+kotlin.js {
+    browser()
+
+    binaries.executable()
 }
 
 val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
 
 dependencies {
     implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
     implementation("com.github.turansky.kfc:kfc-react")
 }
