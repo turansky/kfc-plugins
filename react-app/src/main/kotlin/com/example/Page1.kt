a040e6d6be3d9ea5ddbbe847a86b1444a6e22042
Victor Turansky <victor.turansky@gmail.com>
2022-01-10 06:02:20 +0300

    More content for lazy pages

--- react-app/src/main/kotlin/com/example/Page1.kt
+++ react-app/src/main/kotlin/com/example/Page1.kt
@@ -1,13 +1,34 @@
 package com.example
 
+import csstype.Color
 import kfc.react.import
+import kotlinext.js.jso
 import react.FC
 import react.Props
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.section
 
 val LazyPage1 = react.lazy {
     import(Page1)
 }
 
 private val Page1 = FC<Props>("Page1") {
-    +"Page1"
+    section {
+        style = jso {
+            color = Color("#00FF00")
+        }
+
+        +"Page1"
+
+        div { +"A1" }
+        div { +"A2" }
+        div { +"A3" }
+        div { +"A4" }
+        div { +"A5" }
+        div { +"A6" }
+        div { +"A7" }
+        div { +"A8" }
+        div { +"A9" }
+        div { +"A0" }
+    }
 }
--- react-app/src/main/kotlin/com/example/Page2.kt
+++ react-app/src/main/kotlin/com/example/Page2.kt
@@ -1,13 +1,34 @@
 package com.example
 
+import csstype.Color
 import kfc.react.import
+import kotlinext.js.jso
 import react.FC
 import react.Props
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.section
 
 val LazyPage2 = react.lazy {
     import(Page2)
 }
 
 private val Page2 = FC<Props>("Page2") {
-    +"Page2"
+    section {
+        style = jso {
+            color = Color("#FF0000")
+        }
+
+        +"Page2"
+
+        div { +"B1" }
+        div { +"B2" }
+        div { +"B3" }
+        div { +"B4" }
+        div { +"B5" }
+        div { +"B6" }
+        div { +"B7" }
+        div { +"B8" }
+        div { +"B9" }
+        div { +"B0" }
+    }
 }
