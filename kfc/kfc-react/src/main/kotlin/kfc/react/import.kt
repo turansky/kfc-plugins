3dd133a2bea205957aef6deb1801f7e0b743e4cc
Victor Turansky <victor.turansky@gmail.com>
2022-01-10 03:16:56 +0300

    Fix `import` signature

--- kfc/kfc-react/src/main/kotlin/kfc/react/import.kt
+++ kfc/kfc-react/src/main/kotlin/kfc/react/import.kt
@@ -3,8 +3,9 @@ package kfc.react
 import react.ComponentModule
 import react.ComponentType
 import react.Props
+import kotlin.js.Promise
 
 @JsName("__react_component_import__")
 external fun <P : Props> import(
     type: ComponentType<P>,
-): ComponentModule<P>
+): Promise<ComponentModule<P>>
