51faea94524014039ce433cdcb749645c4f3da75
Victor Turansky <victor.turansky@gmail.com>
2022-01-16 20:47:39 +0300

    Safe Node configuration

--- kfc/kfc-gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/LatestNodePlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/com/github/turansky/kfc/gradle/plugin/LatestNodePlugin.kt
@@ -3,8 +3,14 @@ package com.github.turansky.kfc.gradle.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.the
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
+
+private const val YARN_VERSION = "1.22.17"
+private const val NODE_VERSION = "16.13.1"
 
 class LatestNodePlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
@@ -13,6 +19,17 @@ class LatestNodePlugin : Plugin<Project> {
 }
 
 private fun Project.configureLatestNode() {
-    YarnPlugin.apply(this).version = "1.22.17"
-    the<NodeJsRootExtension>().nodeVersion = "16.13.1"
+    plugins.withType<YarnPlugin> {
+        the<YarnRootExtension>().apply {
+            version = YARN_VERSION
+            lockFileDirectory = projectDir
+            ignoreScripts = false
+        }
+    }
+
+    plugins.withType<NodeJsRootPlugin> {
+        the<NodeJsRootExtension>().apply {
+            nodeVersion = NODE_VERSION
+        }
+    }
 }
