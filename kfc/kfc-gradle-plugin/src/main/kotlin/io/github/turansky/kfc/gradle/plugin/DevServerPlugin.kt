36ff93316bade951948899d13298dcddfe7b9723
Victor Turansky <victor.turansky@gmail.com>
2022-02-03 03:32:18 +0300

    Migration from `com.github.*` to `io.github.*`

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/DevServerPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/DevServerPlugin.kt
@@ -1,6 +1,6 @@
 package io.github.turansky.kfc.gradle.plugin
 
-import com.github.turansky.kfc.gradle.plugin.Output.DEV_SERVER
+import io.github.turansky.kfc.gradle.plugin.Output.DEV_SERVER
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/PluginPublishPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/PluginPublishPlugin.kt
@@ -1,6 +1,6 @@
 package io.github.turansky.kfc.gradle.plugin
 
-import com.github.turansky.kfc.gradle.plugin.GradleProperty.VERSION
+import io.github.turansky.kfc.gradle.plugin.GradleProperty.VERSION
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.bundling.Jar
