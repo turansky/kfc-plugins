d5160db57eb432d44ff832d22fb37763273c120a
Victor Turansky <victor.turansky@gmail.com>
2022-02-03 03:50:21 +0300

    Fix compilation

--- react-app/build.gradle.kts
+++ react-app/build.gradle.kts
@@ -8,5 +8,5 @@ dependencies {
     implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
-    implementation("com.github.turansky.kfc:kfc-react")
+    implementation("io.github.turansky.kfc:kfc-react")
 }
