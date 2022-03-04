d004c2f1fbede64e31301c84f3de2f97ff38b426
Victor Turansky <victor.turansky@gmail.com>
2022-03-04 10:12:57 +0300

    Webpack `5.70.0`

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestWebpackPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestWebpackPlugin.kt
@@ -16,7 +16,7 @@ class LatestWebpackPlugin : Plugin<Project> {
 private fun Project.configureLatestWebpack() {
     plugins.withType<NodeJsRootPlugin> {
         the<NodeJsRootExtension>().versions.apply {
-            webpack.version = "5.69.1"
+            webpack.version = "5.70.0"
             webpackCli.version = "4.9.2"
             webpackDevServer.version = "4.7.4"
         }
