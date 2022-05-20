ec16cbb15712783b607b80f85004adbc3ee47d2e
Victor Turansky <victor.turansky@gmail.com>
2022-05-20 03:02:19 +0300

    Yarn `1.22.19`

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestNodePlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestNodePlugin.kt
@@ -9,7 +9,7 @@ import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
-private const val YARN_VERSION = "1.22.18"
+private const val YARN_VERSION = "1.22.19"
 private const val NODE_VERSION = "16.15.0"
 
 class LatestNodePlugin : Plugin<Project> {
