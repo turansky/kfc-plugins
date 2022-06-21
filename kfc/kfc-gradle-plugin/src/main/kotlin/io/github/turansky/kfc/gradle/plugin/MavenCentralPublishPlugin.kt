66282895819047fd5d1fd7b2520347b07a1d753b
Victor Turansky <victor.turansky@gmail.com>
2022-06-21 15:41:55 +0300

    Fix property name

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/MavenCentralPublishPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/MavenCentralPublishPlugin.kt
@@ -3,7 +3,6 @@ package io.github.turansky.kfc.gradle.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
-import org.gradle.api.publish.maven.MavenPom
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.kotlin.dsl.*
@@ -35,8 +34,8 @@ class MavenCentralPublishPlugin : Plugin<Project> {
         fun pomProperty(name: String): String =
             property("kfc.pom.$name") as String
 
-        val jvmMode = plugins.hasPlugin(KotlinPlugin.JVM)
-        val javadocJar = if (jvmMode) {
+        val emptyJavadocRequired = plugins.hasPlugin(KotlinPlugin.JVM)
+        val javadocJar = if (emptyJavadocRequired) {
             tasks.register("emptyJavadocJar", Jar::class) {
                 archiveClassifier.set("javadoc")
             }
