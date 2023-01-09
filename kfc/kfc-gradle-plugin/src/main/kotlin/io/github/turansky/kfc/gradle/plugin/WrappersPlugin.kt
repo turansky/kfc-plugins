770c0efce9d0c77685aeba8a4c49cf32020b8c15
Victor Turansky <victor.turansky@gmail.com>
2023-01-10 01:14:41 +0200

    Use wrappers BOM by default

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/WrappersPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/WrappersPlugin.kt
@@ -2,15 +2,28 @@ package io.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+import org.gradle.kotlin.dsl.dependencies
 
 class WrappersPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
-        extensions.create(
-            WrappersBomDependencyExtension::class.java,
-            "wrappersBom",
-            WrappersBomDependencyExtensionImpl::class.java,
-            project
-        )
+        plugins.withId(KotlinPlugin.MULTIPLATFORM) {
+            dependencies {
+                applyConfiguration(
+                    configurationName = JS_MAIN_IMPLEMENTATION,
+                    wrappersVersion = wrappersVersion(),
+                )
+            }
+        }
+
+        plugins.withId(KotlinPlugin.JS) {
+            dependencies {
+                applyConfiguration(
+                    configurationName = IMPLEMENTATION,
+                    wrappersVersion = wrappersVersion(),
+                )
+            }
+        }
 
         extensions.create(
             WrappersDependencyExtension::class.java,
@@ -18,4 +31,19 @@ class WrappersPlugin : Plugin<Project> {
             WrappersDependencyExtensionImpl::class.java,
         )
     }
+
+    private fun DependencyHandlerScope.applyConfiguration(
+        configurationName: String,
+        wrappersVersion: String,
+    ) {
+        configurationName(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$wrappersVersion"))
+    }
 }
+
+private fun Project.wrappersVersion(): String =
+    version("kotlin-wrappers")
+
+private fun Project.version(
+    target: String,
+): String =
+    property("${target}.version") as String
