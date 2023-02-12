a4184f16aa92b555c88a0f80ffccca175ba62fae
Victor Turansky <victor.turansky@gmail.com>
2023-02-12 05:30:41 +0200

    Revert "Test task optimization removed"
    This reverts commit f694fbaea4403160d8775a9fc9cbb2d1b810e519.

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinDefaults.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinDefaults.kt
@@ -2,7 +2,9 @@ package io.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.getByName
 import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 
 private const val JS_COMPILER = "kotlin.js.compiler"
 private const val BUILD_DISTRIBUTION = "kotlin.js.generate.executable.default"
@@ -32,6 +34,7 @@ internal fun Project.applyKotlinDefaults(
     plugins.apply(ValueClassSerializationPlugin::class)
 
     configureStrictMode()
+    disableTestsWithoutSources()
 
     extensions.create(
         NpmvDependencyExtension::class.java,
@@ -47,4 +50,43 @@ private fun Project.configureStrictMode() {
             kotlinOptions.allWarningsAsErrors = true
         }
     }
+}
+
+private fun Project.disableTestsWithoutSources() {
+    afterEvaluate {
+        val sourceSet = SourceSet.values()
+            .single { it.taskNames.mapNotNull(tasks::findByPath).isNotEmpty() }
+
+        sourceSet.taskNames
+            .mapNotNull(tasks::findByPath)
+            .forEach {
+                it.onlyIf {
+                    val kotlin = project.extensions.getByName<KotlinProjectExtension>("kotlin")
+                    sourceSet.names
+                        .asSequence()
+                        .map { kotlin.sourceSets.getByName(it) }
+                        .mapNotNull { it.kotlin.sourceDirectories.singleOrNull() }
+                        .any { it.exists() }
+                }
+            }
+    }
+}
+
+private enum class SourceSet(
+    val names: Set<String>,
+    val taskPrefixes: Set<String>,
+) {
+    MULTIPLATFORM(
+        names = setOf("jsTest", "commonTest"),
+        taskPrefixes = setOf("jsTest", "jsIrTest", "jsLegacyTest")
+    ),
+
+    JS(
+        names = setOf("test"),
+        taskPrefixes = setOf("test", "irTest", "legacyTest")
+    ),
+
+    ;
+
+    val taskNames = taskPrefixes.map { "${it}PackageJson" }
 }
