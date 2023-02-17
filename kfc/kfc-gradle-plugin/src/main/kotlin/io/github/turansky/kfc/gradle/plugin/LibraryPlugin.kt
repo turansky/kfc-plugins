99f0faae3f9b5a049e8f46ba651098ec730a6444
Victor Turansky <victor.turansky@gmail.com>
2023-02-17 02:17:52 +0200

    `library` plugin is multiplatform

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LibraryPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LibraryPlugin.kt
@@ -2,9 +2,11 @@ package io.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 
 class LibraryPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
-        applyKotlinJsPlugin()
+        plugins.apply(MultiplatformPlugin::class)
+        plugins.apply(WebpackPlugin::class)
     }
 }
