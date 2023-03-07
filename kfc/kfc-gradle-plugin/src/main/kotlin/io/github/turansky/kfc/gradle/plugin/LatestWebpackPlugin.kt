1399ccc7b87ae24f97cedb8f43a15c9bd0901d1f
Victor Turansky <victor.turansky@gmail.com>
2023-03-07 22:05:16 +0200

    Remove dev server configuration

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestWebpackPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestWebpackPlugin.kt
@@ -7,8 +7,6 @@ import org.gradle.kotlin.dsl.the
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
-import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
-import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 class LatestWebpackPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
@@ -22,15 +20,8 @@ private class RootLatestWebpackPlugin : Plugin<Project> {
             the<NodeJsRootExtension>().versions.apply {
                 webpack.version = "5.75.0"
                 webpackCli.version = "4.10.0"
-                webpackDevServer.version = "4.11.1"
 
                 karma.version = "6.4.1"
-            }
-        }
-
-        plugins.withType<YarnPlugin> {
-            the<YarnRootExtension>().apply {
-                resolution("nanoid", "^3.3.4")
             }
         }
     }
