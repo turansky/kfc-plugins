566d6fcd98caadfac506dacad3c67244cdb708ff
Victor Turansky <victor.turansky@gmail.com>
2023-03-15 17:04:32 +0200

    Webpack `5.76.1` (security update)

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestWebpackPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestWebpackPlugin.kt
@@ -18,7 +18,7 @@ private class RootLatestWebpackPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         plugins.withType<NodeJsRootPlugin> {
             the<NodeJsRootExtension>().versions.apply {
-                webpack.version = "5.75.0"
+                webpack.version = "5.76.1"
                 webpackCli.version = "4.10.0"
 
                 karma.version = "6.4.1"
