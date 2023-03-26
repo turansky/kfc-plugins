eb1bc88d63948f6421dd97a86eb02c7e15532d2d
Victor Turansky <victor.turansky@gmail.com>
2023-03-26 17:40:30 +0300

    Remove wrappers dependencies

--- gradle-plugin-test/build.gradle.kts
+++ gradle-plugin-test/build.gradle.kts
@@ -2,12 +2,7 @@ plugins {
     id("io.github.turansky.kfc.library")
 }
 
-val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
-
 dependencies {
-    jsMainImplementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
-    jsMainImplementation("org.jetbrains.kotlin-wrappers:kotlin-react")
-
     jsTestImplementation(kotlin("test-js"))
 }
 
