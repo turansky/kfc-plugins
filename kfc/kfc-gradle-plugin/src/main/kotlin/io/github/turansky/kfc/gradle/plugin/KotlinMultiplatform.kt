899895330f3e95ba3b0e449e3e115ae51b3a2b90
Victor Turansky <victor.turansky@gmail.com>
2023-04-11 22:05:37 +0300

    Revert "No ES mode by default"
    This reverts commit 22b0ac5fb7f93bb22de9c741ffdf035b1bc83c6c.

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinMultiplatform.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinMultiplatform.kt
@@ -7,7 +7,7 @@ import org.gradle.kotlin.dsl.the
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
-private val ES_MODE = BooleanProperty("kfc.es.mode", false)
+private val ES_MODE = BooleanProperty("kfc.es.mode", true)
 
 internal fun Project.applyKotlinMultiplatformPlugin(
     binaries: Boolean = false,
