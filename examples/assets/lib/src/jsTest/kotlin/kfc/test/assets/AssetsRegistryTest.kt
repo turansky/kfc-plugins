c8e7d50d3ad7adc70b77403dbd860167d28f2c6b
Victor Turansky <victor.turansky@gmail.com>
2023-05-14 21:58:54 +0300

    Add `AssetRegistry` tests

--- examples/assets/lib/src/jsTest/kotlin/kfc/test/assets/AssetsRegistryTest.kt
+++ examples/assets/lib/src/jsTest/kotlin/kfc/test/assets/AssetsRegistryTest.kt
@@ -1,15 +1,16 @@
 package kfc.test.assets
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class AssetsRegistryTest {
     @Test
     fun oneLine() {
-
+        assertEquals(AssetRegistry["system/close"], "<close/>")
     }
 
     @Test
     fun multiLine() {
-
+        assertEquals(AssetRegistry["multiline/close"], "<close><child/></close>")
     }
 }
