61789739b5b9acc84a7b817a5b154ca654662164
Victor Turansky <victor.turansky@gmail.com>
2023-05-24 15:31:59 +0300

    Revert "Restore coroutines error handling patch"
    This reverts commit 3ff919d48c34d1fce44d761e84300268e361e991.

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/CoroutinesErrorHandlingPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/CoroutinesErrorHandlingPlugin.kt
@@ -23,63 +23,8 @@ private const val NEW_COROUTINES_ERROR_HANDLING: String = """
 class CoroutinesErrorHandlingPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         tasks.configureEach<PatchWebpackConfig> {
-            patch(
-                name = "coroutine-error-handling",
-                body = createReplacePatch(
-                    StringReplacement(OLD_COROUTINES_ERROR_HANDLING, NEW_COROUTINES_ERROR_HANDLING),
-                    StringReplacement(OLD_COROUTINES_ERROR_HANDLING.trimIndent(), NEW_COROUTINES_ERROR_HANDLING),
-                )
-            )
+            replace(OLD_COROUTINES_ERROR_HANDLING, NEW_COROUTINES_ERROR_HANDLING)
+            replace(OLD_COROUTINES_ERROR_HANDLING.trimIndent(), NEW_COROUTINES_ERROR_HANDLING)
         }
     }
-}
-
-private data class StringReplacement(
-    private val oldValue: String,
-    private val newValue: String,
-) {
-    val search: String = multiline(escape(oldValue))
-    val replace: String = multiline(newValue)
-    val flags: String = listOfNotNull(
-        "g",
-        "m".takeIf { "\n" in oldValue },
-    ).joinToString("")
-
-    companion object {
-        private fun multiline(
-            source: String,
-        ): String =
-            source.replace("\n", """\n""")
-
-        private const val SPECIAL_SYMBOLS = "(){}.,"
-
-        private fun escape(source: String): String =
-            SPECIAL_SYMBOLS.asSequence()
-                .fold(source) { acc, char ->
-                    acc.replace(char.toString(), """\\$char""")
-                }
-    }
-}
-
-private fun createReplacePatch(
-    vararg replacements: StringReplacement,
-): String {
-    val replacementOptions = replacements.map { r ->
-        """{ search: "${r.search}", replace: "${r.replace}", flags : "${r.flags}" },"""
-    }.joinToString("\n                ")
-
-    // language=JavaScript
-    return """
-        config.module.rules.push(
-          {
-            test: /\.js${'$'}/,
-            loader: 'string-replace-loader',
-            options: {
-              multiple: [
-                $replacementOptions
-              ]
-            }
-          },
-        )
-    """.trimIndent()
 }
