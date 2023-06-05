c26184776810f88a1a7f06c948489efa0233a54a
Victor Turansky <victor.turansky@gmail.com>
2023-06-05 23:13:06 +0300

    Add common targets

--- examples/assets/lib-multiplatform/build.gradle.kts
+++ examples/assets/lib-multiplatform/build.gradle.kts
@@ -2,6 +2,29 @@ plugins {
     id("io.github.turansky.kfc.library")
 }
 
+kotlin {
+    js()
+    iosX64()
+
+    sourceSets {
+        val clientCommonMain by creating {
+            // do nothing
+        }
+
+        val mobileCommonMain by creating {
+            dependsOn(clientCommonMain)
+        }
+
+        val jsMain by getting {
+            dependsOn(clientCommonMain)
+        }
+
+        val iosX64Main by getting {
+            dependsOn(mobileCommonMain)
+        }
+    }
+}
+
 dependencies {
     jsTestImplementation(kotlin("test-js"))
 }
