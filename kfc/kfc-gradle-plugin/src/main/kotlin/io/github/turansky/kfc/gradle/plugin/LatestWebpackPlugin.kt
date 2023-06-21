cae5092a40e8cf164a10a3cfcf90c2626e6efc82
Victor Turansky <victor.turansky@gmail.com>
2023-06-22 02:18:50 +0300

    Webpack `5.88.0`

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestWebpackPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestWebpackPlugin.kt
@@ -18,7 +18,7 @@ private class RootLatestWebpackPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         plugins.withType<NodeJsRootPlugin> {
             the<NodeJsRootExtension>().versions.apply {
-                webpack.version = "5.87.0"
+                webpack.version = "5.88.0"
                 webpackCli.version = "5.1.4"
 
                 karma.version = "6.4.2"
