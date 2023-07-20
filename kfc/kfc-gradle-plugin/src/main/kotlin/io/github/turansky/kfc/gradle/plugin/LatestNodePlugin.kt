83b40dce78c286bd9af4416bb2c45761a75b996c
Victor Turansky <victor.turansky@gmail.com>
2023-07-20 22:27:12 +0300

    Node `18.17.0`

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestNodePlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestNodePlugin.kt
@@ -12,7 +12,7 @@ import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 private const val YARN_VERSION = "1.22.19"
-private const val NODE_VERSION = "18.16.1"
+private const val NODE_VERSION = "18.17.0"
 
 class LatestNodePlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
