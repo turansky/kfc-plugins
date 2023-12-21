38b3bc7ac6b95090de46c99e43e6e4f65aa19870
Victor Turansky <victor.turansky@gmail.com>
2023-12-21 21:23:36 +0200

    Ignore async resource test

--- examples/resources/app-d/src/jsTest/kotlin/AppTest.kt
+++ examples/resources/app-d/src/jsTest/kotlin/AppTest.kt
@@ -1,9 +1,11 @@
 import com.test.view.Api
 import js.promise.await
 import kotlinx.coroutines.test.runTest
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@Ignore
 class AppTest {
     @Test
     fun whatIsTheMeaningOfLife() = runTest {
