cad177d158b21c207a1e9e6112ac785d74f16f7b
porotkin <preload737773@gmail.com>
2024-04-09 19:06:34 +0400

    Add custom plugin for applying WA

---
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/PackageJsonMainFixPlugin.kt
@@ -0,0 +1,23 @@
+package io.github.turansky.kfc.gradle.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.the
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
+// WA: For ESM bundles there's incorrect package.json "main" field defined
+class PackageJsonMainFixPlugin : Plugin<Project> {
+    override fun apply(target: Project): Unit = with(target) {
+        val kotlin = the<KotlinMultiplatformExtension>()
+
+        // WA: For ESM bundles there's incorrect package.json "main" field defined
+        kotlin.js().compilations.forEach {
+            it.packageJson {
+                val mainField = main?.removeSuffix(".js")
+                if (!mainField.isNullOrEmpty() && !mainField.endsWith(".mjs")) {
+                    this.main = mainField + ".mjs"
+                }
+            }
+        }
+    }
+}
