b531c2f00c8562c572c7e7c2e6ef7203f10d4a6c
porotkin <preload737773@gmail.com>
2024-04-10 19:40:03 +0400

    Add dynamic import for view-wl

--- examples/web-worker/local-server/build.gradle.kts
+++ examples/web-worker/local-server/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     id("io.github.turansky.kfc.application")
+    id("io.github.turansky.kfc.wrappers")
 }
 
 dependencies {
     jsMainModule(project(":examples:web-worker:view-wl"))
+    jsMainImplementation(wrappers("js"))
 }
--- examples/web-worker/local-server/src/jsMain/kotlin/com/test/local/Local.kt
+++ examples/web-worker/local-server/src/jsMain/kotlin/com/test/local/Local.kt
@@ -1,6 +1,10 @@
 package com.test.local
 
+import js.import.Module
+import js.import.import
+
 // For dev server only
 private fun main() {
     println("Local!")
+    import<Module<*>>("ww-view-wl")
 }
