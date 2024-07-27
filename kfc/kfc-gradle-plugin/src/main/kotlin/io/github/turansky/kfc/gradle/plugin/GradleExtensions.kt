5d6610dc984191995bd778106e9a2abe531ecd8a
Victor Turansky <victor.turansky@gmail.com>
2024-07-28 01:59:18 +0300

    Cleanup

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/GradleExtensions.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/GradleExtensions.kt
@@ -1,7 +1,6 @@
 package io.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.DomainObjectCollection
-import org.gradle.api.Task
 import org.gradle.kotlin.dsl.withType
 
 // https://github.com/gradle/gradle/issues/9832
@@ -9,20 +8,4 @@ internal inline fun <reified S : Any> DomainObjectCollection<in S>.configureEach
     noinline action: S.() -> Unit,
 ) {
     withType().configureEach(action)
-}
-
-internal inline fun <reified S : Task> DomainObjectCollection<in S>.disable() {
-    configureEach<S> {
-        enabled = false
-    }
-}
-
-internal inline fun <reified S : Task> DomainObjectCollection<in S>.disable(
-    noinline predicate: S.() -> Boolean,
-) {
-    configureEach<S> {
-        if (predicate()) {
-            enabled = false
-        }
-    }
 }
