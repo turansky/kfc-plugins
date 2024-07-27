fc0b856d9fec04937bef02d8378c78e6670e6a14
Victor Turansky <victor.turansky@gmail.com>
2024-07-28 02:12:16 +0300

    Move `eachModuleProjectDependency` in separate file

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Tasks.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Tasks.kt
@@ -1,17 +1,7 @@
 package io.github.turansky.kfc.gradle.plugin
 
-import org.gradle.api.Project
-import org.gradle.api.Task
-
 internal const val DEFAULT_TASK_GROUP: String = "KFC"
 
 internal const val COMPILE_PRODUCTION: String = "compileProductionExecutableKotlinJs"
 internal const val COMPILE_DEVELOPMENT: String = "compileDevelopmentExecutableKotlinJs"
 internal const val KOTLIN_SOURCES_TASK = "kotlinSourcesJar"
-
-internal fun Task.eachModuleProjectDependency(
-    action: (Project) -> Unit,
-) {
-    project.relatedModuleProjects()
-        .forEach(action)
-}
