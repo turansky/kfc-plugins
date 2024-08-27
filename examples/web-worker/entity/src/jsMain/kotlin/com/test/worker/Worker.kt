944f386b7152e077717e045f1855c3621d29629b
Victor Turansky <victor.turansky@gmail.com>
2024-08-27 03:30:12 +0300

    Wrappers `pre.798`

--- examples/web-worker/entity/src/jsMain/kotlin/com/test/worker/Worker.kt
+++ examples/web-worker/entity/src/jsMain/kotlin/com/test/worker/Worker.kt
@@ -1,6 +1,5 @@
 package com.test.worker
 
-import web.events.EventTarget
 import web.events.addEventHandler
 import web.messaging.MessageEvent
 import web.workers.Worker
@@ -8,7 +7,7 @@ import web.workers.Worker
 fun Worker.addMessageHandler(
     handler: Message.() -> Unit,
 ): () -> Unit {
-    return addEventHandler(MessageEvent.message<Message, EventTarget>()) {
+    return addEventHandler(MessageEvent.message<Message>()) {
         handler(it.data)
     }
 }
