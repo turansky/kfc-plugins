35a56f089838fbf04cf033bca9466ea96b736b47
porotkin <preload737773@gmail.com>
2024-08-29 00:59:15 +0300

    Vite. Bundle Test refactoring

--- examples/vite/simple-app-test/src/jvmTest/kotlin/BundleTest.kt
+++ examples/vite/simple-app-test/src/jvmTest/kotlin/BundleTest.kt
@@ -1,14 +1,19 @@
 import java.io.File
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertContains
 import kotlin.test.assertNotNull
 
 class BundleTest {
+    private val path: String? = System.getenv("BUNDLE_PATH")
+
+    @BeforeTest
+    fun checkPath() {
+        assertNotNull(path)
+    }
+
     @Test
     fun `check webpack bundle`() {
-        val path = System.getenv("BUNDLE_PATH")
-        assertNotNull(path)
-
         val projectName = "simple-webpack-app"
 
         val file = File("$path/$projectName/$projectName.js")
@@ -17,9 +22,6 @@ class BundleTest {
 
     @Test
     fun `check vite bundle`() {
-        val path = System.getenv("BUNDLE_PATH")
-        assertNotNull(path)
-
         val projectName = "simple-vite-app"
 
         val file = File("$path/$projectName/$projectName.js")
