2d09af158ebc7263f1dbd7f7d75e0d2bf1112c48
Victor Turansky <victor.turansky@gmail.com>
2024-08-31 00:50:35 +0300

    Vite. Mark output file as output

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinViteTask.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinViteTask.kt
@@ -1,12 +1,15 @@
 package io.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import org.jetbrains.kotlin.gradle.targets.js.NpmPackageVersion
 import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrCompilation
 import org.jetbrains.kotlin.gradle.targets.js.npm.RequiresNpmDependencies
 import org.jetbrains.kotlin.gradle.targets.js.npm.npmProject
+import java.io.File
 
 private val VITE = NpmPackageVersion("vite", "5.4.2")
 private const val VITE_BIN = "vite/bin/vite.js"
@@ -14,8 +17,12 @@ private const val VITE_BIN = "vite/bin/vite.js"
 abstract class KotlinViteTask : DefaultTask(), RequiresNpmDependencies {
 
     @get:Internal
-    override val compilation: KotlinJsIrCompilation =
+    final override val compilation: KotlinJsIrCompilation =
         project.kotlinJsMainCompilation()
+
+    @get:OutputFile
+    open val configFile: Provider<File> =
+        compilation.npmProject.dir.map { it.file("vite.config.js").asFile }
 
     @get:Internal
     override val requiredNpmDependencies =
@@ -23,9 +30,7 @@ abstract class KotlinViteTask : DefaultTask(), RequiresNpmDependencies {
 
     @TaskAction
     private fun build() {
-        compilation.npmProject.dir.get()
-            .file("vite.config.js").asFile
-            .writeText(getViteConfig(project))
+        configFile.get().writeText(getViteConfig(project))
 
         project.exec {
             compilation.npmProject.useTool(
