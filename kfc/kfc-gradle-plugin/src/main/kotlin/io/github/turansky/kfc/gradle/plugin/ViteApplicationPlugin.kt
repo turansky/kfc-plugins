28d4e7a1bc95664c1758d763948b273588258b77
Victor Turansky <victor.turansky@gmail.com>
2024-08-31 01:26:23 +0300

    Vite. Initial plugin

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/ViteApplicationPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/ViteApplicationPlugin.kt
@@ -2,21 +2,16 @@ package io.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Sync
 import org.gradle.kotlin.dsl.create
 
 class ViteApplicationPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
-        tasks.create<Sync>(Vite.productionTask) {
-            from(tasks.named(COMPILE_PRODUCTION)) {
-                rename("(.+).mjs", "$1.js")
-            }
-
-            into(temporaryDir)
+        tasks.create<KotlinViteTask>(Vite.productionTask) {
+            outputDirectory.convention(getProductionDistDirectory())
         }
 
-        tasks.create(Vite.developmentTask) {
-
+        tasks.create<KotlinViteTask>(Vite.developmentTask) {
+            outputDirectory.convention(getProductionDistDirectory())
         }
     }
 }
