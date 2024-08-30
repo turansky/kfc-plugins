21ef9dac744077fdbb85332e17dd704c3e2e3741
Victor Turansky <victor.turansky@gmail.com>
2024-08-31 00:41:31 +0300

    Vite. Config function

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/ViteConfig.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/ViteConfig.kt
@@ -8,8 +8,8 @@ import org.gradle.api.Project
 fun getViteConfig(
     project: Project,
 ): String = """
-import {resolve} from 'node:path'
-import {defineConfig} from 'vite'
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
     build: {
