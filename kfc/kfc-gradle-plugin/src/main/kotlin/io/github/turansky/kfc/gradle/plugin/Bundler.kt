3f847093100bdfaacb1be8c8dfba5016f6035fb6
porotkin <preload737773@gmail.com>
2024-08-30 20:29:55 +0300

    Bundler. Add task names

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Bundler.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Bundler.kt
@@ -1,8 +1,17 @@
 package io.github.turansky.kfc.gradle.plugin
 
-enum class Bundler {
-    VITE,
-    WEBPACK,
+enum class Bundler(
+    productionTask: String,
+    developmentTask: String,
+) {
+    VITE(
+        productionTask = "jsBrowserProductionVite",
+        developmentTask = "jsBrowserDevelopmentVite",
+    ),
+    WEBPACK(
+        productionTask = Webpack.PRODUCTION_TASK,
+        developmentTask = Webpack.DEVELOPMENT_TASK,
+    ),
 
     ;
 }
