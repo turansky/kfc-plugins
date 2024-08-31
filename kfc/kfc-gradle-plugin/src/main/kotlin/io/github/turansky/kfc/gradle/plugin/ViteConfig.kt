ad65c506109d7065bce4cc04eaa675c851f01633
Victor Turansky <victor.turansky@gmail.com>
2024-08-31 15:56:06 +0300

    Vite. Support parametrized config

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/ViteConfig.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/ViteConfig.kt
@@ -5,11 +5,43 @@ package io.github.turansky.kfc.gradle.plugin
 import org.gradle.api.Project
 import org.gradle.api.file.Directory
 
-// language=javascript
 fun getViteConfig(
     project: Project,
     mode: ViteMode,
     outputDirectory: Directory,
+): String {
+    val outDir = outputDirectory.asFile.absolutePath
+    val mainPath = "kotlin/${project.jsModuleName}.mjs"
+    val sourceMaps = project.property(SOURCE_MAPS)
+
+    val configFileTemplate = project.layout.projectDirectory
+        .file(Vite.configFile)
+        .asFile
+
+    if (configFileTemplate.exists()) {
+        return configFileTemplate.readText()
+            .replace("%MODE%", mode.toString())
+            .replace("%OUT_DIR%", outDir)
+            .replace("%MAIN%", mainPath)
+            .replace("'%SOURCE_MAP%'", sourceMaps.toString())
+    }
+
+    return getDefaultViteConfig(
+        project = project,
+        mode = mode,
+        outDir = outDir,
+        mainPath = mainPath,
+        sourceMaps = sourceMaps,
+    )
+}
+
+// language=javascript
+private fun getDefaultViteConfig(
+    project: Project,
+    mode: ViteMode,
+    outDir: String,
+    mainPath: String,
+    sourceMaps: Boolean,
 ): String = """
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
@@ -18,15 +50,15 @@ export default defineConfig({
     mode: '$mode',
     build: {
         root: 'kotlin',
-        outDir: '${outputDirectory.asFile.absolutePath}',
+        outDir: '${outDir}',
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                'main': resolve(__dirname, 'kotlin/${project.jsModuleName}.mjs'),
+                'main': resolve(__dirname, '$mainPath'),
             },
             output: {
                 entryFileNames: '${project.jsOutputFileName}',
-                sourcemap: ${project.property(SOURCE_MAPS)},
+                sourcemap: ${sourceMaps},
             },
         },
     },
