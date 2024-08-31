d588cd61155a326c34905277eb3e7b82d0d78cd4
Victor Turansky <victor.turansky@gmail.com>
2024-08-31 15:28:38 +0300

    Vite. Cleanup test project

--- examples/vite/task-check/build.gradle.kts
+++ examples/vite/task-check/build.gradle.kts
@@ -1,19 +1,7 @@
-import io.github.turansky.kfc.gradle.plugin.KotlinViteTask
-
 plugins {
     alias(libs.plugins.kfc.application)
 }
 
 dependencies {
     jsMainImplementation(projects.examples.vite.simpleLibrary)
-}
-
-val viteBuild by tasks.creating(KotlinViteTask::class) {
-    outputDirectory = layout.buildDirectory.dir("my-vite-distribution")
-
-    dependsOn(tasks.compileProductionExecutableKotlinJs)
-}
-
-tasks.build {
-    dependsOn(viteBuild)
 }
