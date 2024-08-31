80d8ac157030289ae73cc08fcad5ef0926e4120a
Victor Turansky <victor.turansky@gmail.com>
2024-08-31 18:39:42 +0300

    Implement `BundlerEnvironmentExtension`

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/BundlerEnvironmentExtension.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/BundlerEnvironmentExtension.kt
@@ -1,3 +1,28 @@
 package io.github.turansky.kfc.gradle.plugin
 
-abstract class BundlerEnvironmentExtension
+import org.gradle.api.Project
+import org.gradle.api.provider.Provider
+
+interface BundlerEnvironmentExtension {
+    fun set(
+        name: String,
+        value: String,
+    )
+}
+
+open class BundlerEnvironmentExtensionImpl(
+    project: Project,
+) : BundlerEnvironmentExtension {
+    private val _variables: MutableList<EnvVariable> = mutableListOf()
+
+    val variables: Provider<List<EnvVariable>> = project.provider {
+        _variables
+    }
+
+    override fun set(
+        name: String,
+        value: String,
+    ) {
+        _variables.add(EnvVariable(name, value))
+    }
+}
--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/BundlerEnvironmentPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/BundlerEnvironmentPlugin.kt
@@ -2,10 +2,14 @@ package io.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.create
 
 class BundlerEnvironmentPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
-        extensions.create<BundlerEnvironmentExtension>("bundlerEnvironment")
+        extensions.create(
+            BundlerEnvironmentExtension::class.java,
+            "bundlerEnvironment",
+            BundlerEnvironmentExtensionImpl::class.java,
+            project,
+        )
     }
 }
