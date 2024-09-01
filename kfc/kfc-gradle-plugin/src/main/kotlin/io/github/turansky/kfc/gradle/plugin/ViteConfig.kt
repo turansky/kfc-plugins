2bc3bf443bfca4bd557bfbed23de1d54aedbcbdb
Victor Turansky <victor.turansky@gmail.com>
2024-09-01 17:13:32 +0300

    Vite. Cleanup default config

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/ViteConfig.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/ViteConfig.kt
@@ -9,7 +9,6 @@ fun getViteConfig(
     project: Project,
     entryFile: RegularFile,
 ): String {
-    val entryName = entryFile.asFile.nameWithoutExtension
     val entryPath = entryFile.asFile.absolutePath
 
     val configFileTemplate = project.layout.projectDirectory
@@ -22,14 +21,12 @@ fun getViteConfig(
     }
 
     return getDefaultViteConfig(
-        entryName = entryName,
         entryPath = entryPath,
     )
 }
 
 // language=javascript
 private fun getDefaultViteConfig(
-    entryName: String,
     entryPath: String,
 ): String = """
 import { defineConfig } from 'vite'
@@ -37,9 +34,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     build: {
         rollupOptions: {
-            input: {
-                '$entryName': '$entryPath',
-            },
+            input: [
+                '$entryPath',
+            ],
             output: {
                 entryFileNames: '[name].js',
             },
