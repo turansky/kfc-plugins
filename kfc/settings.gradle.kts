ca12e79c42705884b9cf133b4998a778bb877fa5
Victor Turansky <victor.turansky@gmail.com>
2024-09-12 23:02:38 +0300

    Publish plugin `1.3.0`

--- kfc/settings.gradle.kts
+++ kfc/settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
         kotlin("jvm") version kotlinVersion
         kotlin("multiplatform") version kotlinVersion
 
-        id("com.gradle.plugin-publish") version "1.2.1"
+        id("com.gradle.plugin-publish") version "1.3.0"
     }
 }
 
