12a84fc1b892a93192665c20cf4f1c429c626184
porotkin <preload737773@gmail.com>
2024-09-26 17:21:30 +0300

    Revert "SingleWebpackCachePlugin. Remove `doFirst` as it breaks worker compilation"
    This reverts commit 4245178593dca16d6d1fa79522a991579089c62a.

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/SingleWebpackCachePlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/SingleWebpackCachePlugin.kt
@@ -2,33 +2,33 @@ package io.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Delete
-import org.gradle.kotlin.dsl.creating
-import org.gradle.kotlin.dsl.getValue
+import org.gradle.api.tasks.TaskContainer
 import org.gradle.kotlin.dsl.named
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 
 class SingleWebpackCachePlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
-        val prodOutput = getProductionDistDirectory()
-        val devOutput = getDevelopmentDistDirectory()
-
-        val deleteRelatedOutputProd by tasks.creating(Delete::class) {
-            delete(devOutput)
-        }
-
-        val deleteRelatedOutputDev by tasks.creating(Delete::class) {
-            delete(prodOutput)
-        }
-
         tasks.named<KotlinWebpack>(Webpack.productionTask) {
-            dependsOn(deleteRelatedOutputProd)
-            outputDirectory.set(prodOutput)
+            outputDirectory.set(getProductionDistDirectory())
         }
 
         tasks.named<KotlinWebpack>(Webpack.developmentTask) {
-            dependsOn(deleteRelatedOutputDev)
-            outputDirectory.set(devOutput)
+            outputDirectory.set(getDevelopmentDistDirectory())
+        }
+
+        tasks.link(Webpack.productionTask, Webpack.developmentTask)
+        tasks.link(Webpack.developmentTask, Webpack.productionTask)
+    }
+
+    private fun TaskContainer.link(
+        taskName: String,
+        relatedTaskName: String,
+    ) {
+        named<KotlinWebpack>(taskName) {
+            doFirst {
+                val relatedTask = named<KotlinWebpack>(relatedTaskName).get()
+                project.delete(relatedTask.outputDirectory)
+            }
         }
     }
 }
