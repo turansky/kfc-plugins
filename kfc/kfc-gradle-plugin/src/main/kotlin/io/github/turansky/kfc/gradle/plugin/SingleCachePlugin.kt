cf225101dc0b91a0cefaca7192d7c804e06af275
porotkin <preload737773@gmail.com>
2024-09-27 17:35:25 +0300

    Revert "SingleCachePlugin. Refactoring"
    This reverts commit 0afa74768c10daa57919987dee4c2c8d2caf9256.

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/SingleCachePlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/SingleCachePlugin.kt
@@ -2,10 +2,10 @@ package io.github.turansky.kfc.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.TaskContainer
+import org.gradle.internal.extensions.stdlib.uncheckedCast
 import org.gradle.kotlin.dsl.register
 
 class SingleCachePlugin : Plugin<Project> {
@@ -21,8 +21,9 @@ class SingleCachePlugin : Plugin<Project> {
         relatedTaskName: String,
     ) {
         val relatedTaskOutputDirectory = named(relatedTaskName).get()
-            .outputDirectory()
-            .get()
+            .property("outputDirectory")
+            ?.uncheckedCast<DirectoryProperty>()
+            ?.get()
 
         val singleCacheTask = "${taskName}SingleCache"
         register<Delete>(singleCacheTask) {
@@ -34,6 +35,3 @@ class SingleCachePlugin : Plugin<Project> {
         }
     }
 }
-
-private fun Task.outputDirectory(): DirectoryProperty =
-    property("outputDirectory") as DirectoryProperty
