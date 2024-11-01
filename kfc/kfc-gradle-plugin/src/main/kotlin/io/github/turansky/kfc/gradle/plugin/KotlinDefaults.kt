f252cec6128adefc42bdc3ad2315005dc0f6b631
Victor Turansky <victor.turansky@gmail.com>
2024-11-02 00:42:22 +0200

    Remove "legacy" mode

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinDefaults.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinDefaults.kt
@@ -9,11 +9,10 @@ private const val DOM_API_INCLUDED = "kotlin.js.stdlib.dom.api.included"
 private const val OUTPUT_GRANULARITY = "kotlin.js.ir.output.granularity"
 
 private val STRICT_MODE = BooleanProperty("kfc.strict.mode", true)
-private val LEGACY = BooleanProperty("kfc.legacy")
 
 internal fun Project.applyKotlinDefaults() {
     ext(DOM_API_INCLUDED, false)
-    ext(OUTPUT_GRANULARITY, if (property(LEGACY)) "whole-program" else "per-file")
+    ext(OUTPUT_GRANULARITY, "per-file")
 
     plugins.apply(SourceMapsPlugin::class)
     plugins.apply(DisableSourcelessTestsPlugin::class)
