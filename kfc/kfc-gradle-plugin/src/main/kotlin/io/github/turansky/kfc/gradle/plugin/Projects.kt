b6bdab100044ca80128a8d8880ad01c9766102ac
Victor Turansky <victor.turansky@gmail.com>
2024-11-06 22:49:53 +0200

    Cleanup

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Projects.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Projects.kt
@@ -6,10 +6,6 @@ private val MODULE_NAME = StringProperty("kfc.module.name")
 
 private val BUNDLER = StringProperty("kfc.bundler")
 
-private val OUTPUT_PATH = StringProperty("kfc.output.path")
-private val OUTPUT_NAME = StringProperty("kfc.output.name")
-private val OUTPUT_HASH = BooleanProperty("kfc.output.hash")
-
 internal val Project.jsModuleName: String
     get() {
         propertyOrNull(MODULE_NAME)
@@ -31,42 +27,6 @@ internal fun Project.getBundler(): Bundler {
         else -> error("Unexpected bundler: $value")
     }
 }
-
-private val Project.jsOutputPath: String?
-    get() = propertyOrNull(OUTPUT_PATH)
-
-internal fun Project.outputPath(
-    path: String,
-): String =
-    outputPath("", path)
-
-internal fun Project.outputPath(
-    prefix: String,
-    suffix: String,
-): String {
-    val basePath = jsOutputPath
-        ?.let { "$it/" }
-        ?: ""
-
-    return prefix + basePath + suffix
-}
-
-internal val Project.jsOutputName: String
-    get() = propertyOrNull(OUTPUT_NAME)
-        ?: jsModuleName
-
-internal val Project.jsOutputFileName: String
-    get() {
-        val hash = if (property(OUTPUT_HASH)) "[contenthash]" else null
-        val path = sequenceOf(jsOutputName, hash, "js")
-            .filterNotNull()
-            .joinToString(".")
-
-        return outputPath(path)
-    }
-
-internal val Project.jsChunkFileName: String
-    get() = outputPath("[name].[contenthash].js")
 
 internal fun Project.ext(
     propertyName: String,
