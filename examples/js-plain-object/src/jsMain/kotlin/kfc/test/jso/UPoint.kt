5801ebd2a0d3b5bcdf0f9e8fc72afacf8991cb91
Victor Turansky <victor.turansky@gmail.com>
2024-12-18 22:44:31 +0200

    Tests

---
+++ examples/js-plain-object/src/jsMain/kotlin/kfc/test/jso/UPoint.kt
@@ -0,0 +1,9 @@
+package kfc.test.jso
+
+import kotlinx.js.JsPlainObject
+
+@JsPlainObject
+external interface UPoint {
+    val x: UInt
+    val y: UInt
+}
---
+++ examples/js-plain-object/src/jsMain/kotlin/kfc/test/jso/UPointInit.kt
@@ -0,0 +1,9 @@
+package kfc.test.jso
+
+import kotlinx.js.JsPlainObject
+
+@JsPlainObject
+external interface UPointInit {
+    val x: UInt?
+    val y: UInt?
+}
