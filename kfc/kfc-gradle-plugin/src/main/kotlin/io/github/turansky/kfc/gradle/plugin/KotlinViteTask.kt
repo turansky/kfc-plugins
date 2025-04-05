053cda187c3538721c56eb8e6e7f00d957b9d4c0
Victor Turansky <victor.turansky@gmail.com>
2025-04-05 16:46:44 +0300

    Vite `6.3.2`

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinViteTask.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinViteTask.kt
@@ -16,7 +16,7 @@ import org.jetbrains.kotlin.gradle.targets.js.npm.RequiresNpmDependencies
 import org.jetbrains.kotlin.gradle.targets.js.npm.npmProject
 import javax.inject.Inject
 
-private val VITE = NpmPackageVersion("vite", "^6.2.5")
+private val VITE = NpmPackageVersion("vite", "^6.3.2")
 
 abstract class KotlinViteTask :
     DefaultTask(),
