e5df45971b335999cd1467f8f2a8905c70c6c1da
Victor Turansky <victor.turansky@gmail.com>
2025-06-24 01:02:14 +0200

    Use suspend `fetch`

--- examples/vite-dev-with-ktor/vite-dev-app/src/jsMain/kotlin/Application.kt
+++ examples/vite-dev-with-ktor/vite-dev-app/src/jsMain/kotlin/Application.kt
@@ -2,15 +2,15 @@ import react.FC
 import react.Props
 import react.useEffectOnce
 import react.useState
-import web.http.fetchAsync
+import web.http.fetch
 
 internal val Application: FC<Props> = FC {
     val (data, setData) = useState<String>()
 
     useEffectOnce {
-        fetchAsync("/api/data").await().textAsync().then {
-            setData(it)
-        }
+        val newData = fetch("/api/data").text()
+
+        setData(newData)
     }
 
     +"Data from Ktor server: $data"
