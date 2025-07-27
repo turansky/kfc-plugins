8b722fb3016c2096b5944a61c32252c6e459c729
Victor Turansky <victor.turansky@gmail.com>
2025-07-27 04:29:55 +0200

    Bundler. Add bundler configurations

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/BundlePlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/BundlePlugin.kt
@@ -10,5 +10,21 @@ import org.gradle.kotlin.dsl.registering
 class BundlePlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         val configuration = Vite.js
+
+        val jsBundleProduction by tasks.registering(Jar::class) {
+            group = DEFAULT_TASK_GROUP
+
+            archiveClassifier.set("js-bundle-production")
+
+            from(tasks.named(configuration.production.buildTask))
+        }
+
+        val jsBundleDevelopment by tasks.registering(Jar::class) {
+            group = DEFAULT_TASK_GROUP
+
+            archiveClassifier.set("js-bundle-development")
+
+            from(tasks.named(configuration.development.buildTask))
+        }
     }
 }
