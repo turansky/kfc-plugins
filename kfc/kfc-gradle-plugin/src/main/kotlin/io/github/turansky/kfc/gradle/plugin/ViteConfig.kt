623c75972ce2742c2b3eb56a9a23922734f76f9a
Victor Turansky <victor.turansky@gmail.com>
2025-07-27 03:20:02 +0200

    Remove `*` import

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/ViteConfig.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/ViteConfig.kt
@@ -9,12 +9,12 @@ internal const val ENTRY_PATH: String = "ENTRY_PATH"
 
 // language=javascript
 private val DEFAULT_VITE_CONFIG: String = """
-import * as process from "node:process";
+import { cwd } from 'node:process'
 import {defineConfig, loadEnv} from 'vite'
 import sourcemaps from 'rollup-plugin-sourcemaps'
 
 export default defineConfig(({mode}) => {
-    const env = loadEnv(mode, process.cwd(), '')
+    const env = loadEnv(mode, cwd(), '')
     return {
         plugins: [sourcemaps()],
         build: {
