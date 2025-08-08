d8635b2d14231e94c307d2e43fa53eade1b47c5a
Victor Turansky <victor.turansky@gmail.com>
2025-08-08 04:12:13 +0200

    Common target

---
+++ examples/vite-build/simple-library/src/commonMain/kotlin/Env.kt
@@ -0,0 +1,7 @@
+@file:JsQualifier("import.meta.env")
+
+@JsName("VITE_BUILD_NAME")
+external val BUILD_NAME: String
+
+@JsName("VITE_BUILD_NUMBER")
+external val BUILD_NUMBER: String
--- examples/vite-build/simple-library/src/jsMain/kotlin/Env.kt
+++
@@ -1,9 +0,0 @@
-@file:Suppress(
-    "NAME_CONTAINS_ILLEGAL_CHARS",
-)
-
-@JsName("import.meta.env.VITE_BUILD_NAME")
-external val BUILD_NAME: String
-
-@JsName("import.meta.env.VITE_BUILD_NUMBER")
-external val BUILD_NUMBER: String
