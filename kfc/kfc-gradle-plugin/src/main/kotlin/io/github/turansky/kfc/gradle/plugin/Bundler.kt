86d487394172d51cdf7f84fc106a65853ed4b9ea
Victor Turansky <victor.turansky@gmail.com>
2025-09-07 03:46:03 +0200

    Vite. Extract bundler config

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Bundler.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Bundler.kt
@@ -36,11 +36,3 @@ class BundlerConfigurationTasks(
     val prepareTask = group + "Prepare"
     val buildTask = group
 }
-
-object Vite : Bundler("Vite") {
-
-    // TODO: We need .mjs extension for now to enable connecting pure ESM plugins
-    //  Until Kotlin isn't specifying `type: "module"` in generated `package.json`
-    //   Ticket: https://youtrack.jetbrains.com/issue/KT-72680/KJS.-Specify-type-module-in-generated-package.json
-    val configFile: String = "vite.config.mjs"
-}
---
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Vite.kt
@@ -0,0 +1,9 @@
+package io.github.turansky.kfc.gradle.plugin
+
+object Vite : Bundler("Vite") {
+
+    // TODO: We need .mjs extension for now to enable connecting pure ESM plugins
+    //  Until Kotlin isn't specifying `type: "module"` in generated `package.json`
+    //   Ticket: https://youtrack.jetbrains.com/issue/KT-72680/KJS.-Specify-type-module-in-generated-package.json
+    val configFile: String = "vite.config.mjs"
+}
