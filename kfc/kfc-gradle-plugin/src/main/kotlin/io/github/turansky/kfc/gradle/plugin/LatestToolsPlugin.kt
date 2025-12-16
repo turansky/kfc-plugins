65ff6e0c44ddeff083e46c871b2cb46265497a01
Victor Turansky <victor.turansky@gmail.com>
2025-12-17 00:04:43 +0100

    Revert "Remove Webpack version configuration"
    This reverts commit 420f07a98b7bd6851f1325a99b683d1ca5188846.

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestToolsPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestToolsPlugin.kt
@@ -5,13 +5,16 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.the
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.targets.js.NpmVersions
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsEnvSpec
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsPlugin
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
 import org.jetbrains.kotlin.gradle.targets.js.npm.LockFileMismatchReport
 import org.jetbrains.kotlin.gradle.targets.js.npm.NpmExtension
 import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsEnvSpec
 import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsPlugin
+import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsRootExtension
 import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsRootPlugin
 import org.jetbrains.kotlin.gradle.targets.wasm.npm.WasmNpmExtension
 
@@ -45,6 +48,8 @@ private class RootLatestToolsPlugin : Plugin<Project> {
         plugins.apply(LatestNodePlugin::class)
 
         plugins.withType<NodeJsRootPlugin> {
+            the<NodeJsRootExtension>().versions.configureVersions()
+
             the<NpmExtension>().apply {
                 lockFileDirectory.set(projectDir)
                 packageLockMismatchReport.set(LockFileMismatchReport.NONE)
@@ -53,10 +58,23 @@ private class RootLatestToolsPlugin : Plugin<Project> {
         }
 
         plugins.withType<WasmNodeJsRootPlugin> {
+            the<WasmNodeJsRootExtension>().versions.configureVersions()
+
             the<WasmNpmExtension>().apply {
                 lockFileDirectory.set(project.layout.buildDirectory.file("wasm-package-lock").get().asFile)
                 packageLockMismatchReport.set(LockFileMismatchReport.NONE)
             }
         }
     }
+}
+
+fun NpmVersions.configureVersions() {
+    // https://www.npmjs.com/package/webpack
+    webpack.version = "5.102.1"
+
+    // https://www.npmjs.com/package/webpack-cli
+    webpackCli.version = "6.0.1"
+
+    // https://www.npmjs.com/package/webpack-dev-server
+    webpackDevServer.version = "5.2.2"
 }
