9403720a7fef96191e5b45319a3c8d7825c5ba0e
Victor Turansky <victor.turansky@gmail.com>
2025-12-21 23:20:50 +0100

    Rolldown Vite `7.3.0`

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Vite.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Vite.kt
@@ -5,7 +5,7 @@ object Vite : Bundler(
     bin = "vite/bin/vite.js",
     dependencies = listOf(
         // https://www.npmjs.com/package/rolldown-vite
-        Dependency(name = "vite", version = "npm:rolldown-vite@^7.2.11"),
+        Dependency(name = "vite", version = "npm:rolldown-vite@^7.3.0"),
     ),
 ) {
 
