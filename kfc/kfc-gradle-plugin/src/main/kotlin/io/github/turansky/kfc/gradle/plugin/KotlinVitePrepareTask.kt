2a0950e7ed16d4fa3c0c3744da4ef7ce484be64f
porotkin <preload737773@gmail.com>
2026-01-02 19:40:39 +0200

    Revert "KotlinVitePrepareTask. Do not create `defaultConfigFile` during task configuration. Use fallback at the execution time instead"
    This reverts commit 3891ce959a2b94ea8a2113a6363ee0ec5903c96c.

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinVitePrepareTask.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/KotlinVitePrepareTask.kt
@@ -33,9 +33,21 @@ abstract class KotlinVitePrepareTask :
     private val workingDirectory: Provider<Directory> =
         npmProject.dir
 
+    private val defaultConfigFile: RegularFileProperty =
+        objectFactory.fileProperty()
+            .convention(::defaultViteConfig)
+
+    private val customConfigFile: RegularFileProperty =
+        objectFactory.fileProperty()
+            .convention(projectDir.file(Vite.CONFIG_FILE))
+
     private val configFile: RegularFileProperty
         get() = objectFactory.fileProperty()
-            .convention(projectDir.file(Vite.CONFIG_FILE))
+            .convention(
+                customConfigFile
+                    .filter { it.asFile.exists() }
+                    .orElse(defaultConfigFile)
+            )
 
     private val entryFile: Provider<RegularFile> =
         workingDirectory.map { it.file("kotlin/${project.jsModuleName}.mjs") }
@@ -50,7 +62,7 @@ abstract class KotlinVitePrepareTask :
 
     @TaskAction
     protected fun sync() {
-        fs.syncFile(configFile, workingDirectory, fallback = ::defaultViteConfig)
+        fs.syncFile(configFile, workingDirectory)
         fs.syncFile(envFile, workingDirectory)
 
         for (fileName in DOT_ENV_FILES) {
