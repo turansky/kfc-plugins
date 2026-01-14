099a35559dc7dfefd2e3f17b581805fb0fbd80e0
Victor Turansky <victor.turansky@gmail.com>
2026-01-14 23:32:20 +0100

    Node `24.13.0`

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestToolsPlugin.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/LatestToolsPlugin.kt
@@ -18,7 +18,7 @@ import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsRootExtension
 import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsRootPlugin
 import org.jetbrains.kotlin.gradle.targets.wasm.npm.WasmNpmExtension
 
-private const val NODE_VERSION = "24.12.0"
+private const val NODE_VERSION = "24.13.0"
 
 private const val YARN = "kotlin.js.yarn"
 
