e8a34d8ab6c1a7b5fba49866d2e6e7d5bbcd4bff
Victor Turansky <victor.turansky@gmail.com>
2026-03-12 22:34:06 +0100

    Examples. "Vite + Ktor" example removed

--- examples/vite-dev-with-ktor/vite-dev-app/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    alias(kfc.plugins.application)
-}
-
-dependencies {
-    commonMainImplementation(kotlinWrappers.browser)
-    commonMainImplementation(kotlinWrappers.react)
-    commonMainImplementation(kotlinWrappers.reactDom)
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,15 +19,6 @@ dependencyResolutionManagement {
             val coroutinesVersion = extra["kotlinx-coroutines.version"] as String
             library("coroutines-core", "org.jetbrains.kotlinx", "kotlinx-coroutines-core").version(coroutinesVersion)
             library("coroutines-test", "org.jetbrains.kotlinx", "kotlinx-coroutines-test").version(coroutinesVersion)
-
-            val ktorVersion = extra["ktor.version"] as String
-            plugin("ktor", "io.ktor.plugin").version(ktorVersion)
-            library("server-core", "io.ktor", "ktor-server-core-jvm").version(ktorVersion)
-            library("server-netty", "io.ktor", "ktor-server-netty-jvm").version(ktorVersion)
-            library("server-html-builder", "io.ktor", "ktor-server-html-builder").version(ktorVersion)
-
-            val logbackVersion = extra["logback.version"] as String
-            library("logback", "ch.qos.logback", "logback-classic").version(logbackVersion)
         }
 
         register("kfc") {
@@ -58,8 +49,5 @@ include("examples:vite-build:simple-app-test")
 include("examples:vite-build:simple-library")
 include("examples:vite-build:simple-vite-app")
 include("examples:vite-dev")
-
-include("examples:vite-dev-with-ktor:ktor-app")
-include("examples:vite-dev-with-ktor:vite-dev-app")
 
 includeBuild("kfc")
