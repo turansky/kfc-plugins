9782a421c1cb6bff2ec4efe11a53de233d969eb9
Victor Turansky <victor.turansky@gmail.com>
2026-04-13 18:55:51 +0200

    Local `@JsQualifier`

--- examples/vite-build/simple-library/src/webMain/kotlin/Env.kt
+++ examples/vite-build/simple-library/src/webMain/kotlin/Env.kt
@@ -1,13 +1,12 @@
-// TEMP
 @file:OptIn(kotlin.js.ExperimentalWasmJsInterop::class)
-
-@file:JsQualifier("import.meta.env")
 
 import kotlin.js.JsName
 import kotlin.js.JsQualifier
 
+@JsQualifier("import.meta.env")
 @JsName("VITE_BUILD_NAME")
 external val BUILD_NAME: String
 
+@JsQualifier("import.meta.env")
 @JsName("VITE_BUILD_NUMBER")
 external val BUILD_NUMBER: String
