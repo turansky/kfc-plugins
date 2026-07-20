880f38fc1d06edd861453d9e664b8e3185036d9b
Victor Turansky <victor.turansky@gmail.com>
2026-07-20 02:06:50 +0200

    Vite `8.1.5`

--- kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Vite.kt
+++ kfc/kfc-gradle-plugin/src/main/kotlin/io/github/turansky/kfc/gradle/plugin/Vite.kt
@@ -5,7 +5,7 @@ object Vite : Bundler(
     bin = "vite/bin/vite.js",
     dependencies = listOf(
         // https://www.npmjs.com/package/vite
-        Dependency(name = "vite", version = "^8.1.4"),
+        Dependency(name = "vite", version = "^8.1.5"),
     ),
 ) {
 
