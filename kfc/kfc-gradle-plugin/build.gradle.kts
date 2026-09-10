ff7947af54fdcc7d973be58e123e95866d993e0d
Victor Turansky <victor.turansky@gmail.com>
2026-09-11 00:57:26 +0200

    Kotlin `2.4.20`

--- kfc/kfc-gradle-plugin/build.gradle.kts
+++ kfc/kfc-gradle-plugin/build.gradle.kts
@@ -72,7 +72,7 @@ enum class KfcPlugin(
     val tags: List<String> = listOf(
         "kotlin",
         "kotlin-js",
-        "javascript"
+        "javascript",
     ) + tags
 }
 
@@ -91,4 +91,9 @@ gradlePlugin {
             }
         }
     }
+}
+
+// TODO: remove after https://github.com/gradle/gradle/issues/38634
+tasks.compileKotlin {
+    compilerOptions.freeCompilerArgs.add("-Xwarning-level=DEPRECATED_CLI_ARG:disabled")
 }
